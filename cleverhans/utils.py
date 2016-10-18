1d3fb7c568efdea6267355210ab800eb996ce459
Zhitao Gong <zhitaao.gong@gmail.com>
2016-10-18 08:22:22 -0500

    simplify the batch index logic

--- cleverhans/utils.py
+++ cleverhans/utils.py
@@ -63,14 +63,6 @@ def batch_indices(batch_nb, data_length, batch_size):
     :param batch_size: the number of inputs in each batch
     :return: pair of (start, end) indices
     """
-    # Batch start and end index
-    start = int(batch_nb * batch_size)
-    end = int((batch_nb + 1) * batch_size)
-
-    # When there are not enough inputs left, we reuse some to complete the batch
-    if end > data_length:
-        shift = end - data_length
-        start -= shift
-        end -= shift
-
+    end = min(int((batch_nb + 1) * batch_size), data_length)
+    start = end - batch_size
     return start, end
