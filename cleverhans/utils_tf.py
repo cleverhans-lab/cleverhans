be58929f5d1da2ed06d50300c087ad24a1830128
npapernot <ngp5056@cse.psu.edu>
2016-11-07 14:09:16 -0500

    add backward compatibility for print and others

--- cleverhans/utils_tf.py
+++ cleverhans/utils_tf.py
@@ -1,3 +1,8 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
 import math
 import numpy as np
 import os
