0f2094d46aab393eb652e67579ab56325dc4d915
npapernot <ngp5056@cse.psu.edu>
2016-11-22 15:12:36 -0500

    fixed success condition check in jsma and added more comments to main function for jsma

--- cleverhans/attacks.py
+++ cleverhans/attacks.py
@@ -230,12 +230,14 @@ def jsma_tf(sess, x, predictions, sample, target, theta, gamma, increase, clip_m
     max_iters = np.floor(np.product(adv_x[0][0].shape) * gamma / 2)
     print('Maximum number of iterations: {0}'.format(max_iters))
 
-    # prep our derivatives for all classes
+    # Define the TF graph elements to compute our derivatives for all classes
     deriv_target, = tf.gradients(predictions[:,target], x)
     other_classes = [i for i in range(FLAGS.nb_classes) if i != target]
     deriv_others, = tf.gradients([predictions[:,i] for i in other_classes], x)
 
-    # compute our search domain based on maximizing or minimizing pixels
+    # Compute our initial search domain. We optimize the initial search domain
+    # by removing all features that are already at their maximum values (if
+    # increasing input features---otherwise, at their minimum value).
     if increase:
         search_domain = set([(row, col) for row in xrange(FLAGS.img_rows) \
                  for col in xrange(FLAGS.img_cols) if adv_x[0, 0, row, col] < clip_max])
@@ -243,34 +245,40 @@ def jsma_tf(sess, x, predictions, sample, target, theta, gamma, increase, clip_m
         search_domain = set([(row, col) for row in xrange(FLAGS.img_rows) \
                  for col in xrange(FLAGS.img_cols) if adv_x[0, 0, row, col] > clip_min])
 
-    # repeat until we have achieved misclassification
+    # Initial the loop variables
     iteration = 0
     current = model_argmax(sess, x, predictions, adv_x)	
+    
+    # Repeat this main loop until we have achieved misclassification
     while current != target and iteration < max_iters and len(search_domain) > 0: 
 
-        # compute the Jacobian derivatives
+        # Compute the Jacobian components
         grads_target, grads_others = jacobian(sess, x, deriv_target, deriv_others, adv_x)
 
-        # compute the salency map for each of our taget classes
+        # Compute the saliency map for each of our target classes
+        # and return the two best candidate features for perturbation
         i, j, search_domain = saliency_map(grads_target, grads_others, search_domain, increase)
 
-        # apply an adversarial perterbation to the sample
+        # Apply the perturbation to the two input features selected previously
         adv_x = apply_perturbations(i, j, adv_x, increase, theta, clip_min, clip_max)
 
-        # update our current prediction
+        # Update our current prediction by querying the model
         current = model_argmax(sess, x, predictions, adv_x)
+        
+        # Update loop variables
         iteration = iteration + 1
 
+        # This process may take a while, so outputting progress regularly
         if iteration % 5 == 0:
             print('Current iteration: {0} - Current Prediction: {1}'.format(iteration, current))
 
-    percent_perterbed = float(iteration * 2)/float(FLAGS.img_rows * FLAGS.img_cols)
-    # failed to perterb the input sample to the target class within the constraints
-    if iteration == max_iters or len(search_domain) == 0:
-        print('Unsuccesful')
-        return adv_x, -1, percent_perterbed
-    # success!
-    else:
-        print('Successful')
-        return adv_x, 1, percent_perterbed
+    # Compute the ratio of pixels perturbed by the algorithm
+    percent_perturbed = float(iteration * 2)/float(FLAGS.img_rows * FLAGS.img_cols)
 
+    # Report success when the adversarial example is misclassified in the target class
+    if current == target:
+        print('Successful')
+        return adv_x, 1, percent_perturbed
+    else:
+        print('Unsuccesful')
+        return adv_x, -1, percent_perturbed
