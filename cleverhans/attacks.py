9ccb0c57bf697505ab24adb538aa0c1508391c03
Karen Hambardzumyan <mahnerak@gmail.com>
2016-12-08 02:54:54 +0400

    Theano versions of FGSM and MNIST tutorial

--- cleverhans/attacks.py
+++ cleverhans/attacks.py
@@ -35,48 +35,12 @@ def fgsm(x, predictions, eps, back='tf', clip_min=None, clip_max=None):
     """
     if back == 'tf':
         # Compute FGSM using TensorFlow
+        from .attacks_tf import fgsm as fgsm_tf
         return fgsm_tf(x, predictions, eps, clip_min=clip_min, clip_max=clip_max)
     elif back == 'th':
-        raise NotImplementedError("Theano FGSM not implemented.")
-
-
-def fgsm_tf(x, predictions, eps, clip_min=None, clip_max=None):
-    """
-    TensorFlow implementation of the Fast Gradient
-    Sign method.
-    :param x: the input placeholder
-    :param predictions: the model's output tensor
-    :param eps: the epsilon (input variation parameter)
-    :param clip_min: optional parameter that can be used to set a minimum
-                    value for components of the example returned
-    :param clip_max: optional parameter that can be used to set a maximum
-                    value for components of the example returned
-    :return: a tensor for the adversarial example
-    """
-
-    # Compute loss
-    y = tf.to_float(tf.equal(predictions, tf.reduce_max(predictions, 1, keep_dims=True)))
-    y = y / tf.reduce_sum(y, 1, keep_dims=True)
-    loss = utils_tf.tf_model_loss(y, predictions, mean=False)
-
-    # Define gradient of loss wrt input
-    grad, = tf.gradients(loss, x)
-
-    # Take sign of gradient
-    signed_grad = tf.sign(grad)
-
-    # Multiply by constant epsilon
-    scaled_signed_grad = eps * signed_grad
-
-    # Add perturbation to original example to obtain adversarial example
-    adv_x = tf.stop_gradient(x + scaled_signed_grad)
-
-    # If clipping is needed, reset all values outside of [clip_min, clip_max]
-    if (clip_min is not None) and (clip_max is not None):
-        adv_x = tf.clip_by_value(adv_x, clip_min, clip_max)
-
-    return adv_x
-
+        # Compute FGSM using Theano
+        from .attack_th import fgsm as fgsm_th
+        raise fgsm_th(x, predictions, eps, clip_min=clip_min, clip_max=clip_max)
 
 def jsma(sess, x, predictions, grads, sample, target, theta, gamma=np.inf, increase=True, back='tf', clip_min=None, clip_max=None):
     """
---
+++ tutorials/mnist_tutorial_th.py
@@ -0,0 +1,98 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import argparse
+
+import theano
+import theano.tensor as T
+
+
+from cleverhans.utils_mnist import data_mnist, model_mnist
+from cleverhans.utils_th import th_model_train, th_model_eval, batch_eval
+from cleverhans.attacks_th import fgsm
+
+
+def main():
+    """
+    MNIST cleverhans tutorial
+    :return:
+    """
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--batch_size', '-b', default=128, help='Size of training batches')
+    parser.add_argument('--train_dir', '-d', default='/tmp', help='Directory storing the saved model.')
+    parser.add_argument('--filename', '-f',  default='mnist.ckpt', help='Filename to save model under.')
+    parser.add_argument('--nb_epochs', '-e', default=6, type=int, help='Number of epochs to train model')
+    parser.add_argument('--learning_rate', '-lr', default=0.1, type=float, help='Learning rate for training')
+    args = parser.parse_args()
+
+    # Get MNIST test data
+    X_train, Y_train, X_test, Y_test = data_mnist()
+    print("Loaded MNIST test data.")
+
+    assert Y_train.shape[1] == 10.
+    label_smooth = .1
+    Y_train = Y_train.clip(label_smooth / 9., 1. - label_smooth)
+
+    # Define input Theano placeholder
+    x_shape = (None, 1, 28, 28)
+    y_shape = (None, 10)
+    x = T.tensor4('x')
+    y = T.matrix('y')
+
+    # Define Theano model graph
+    model = model_mnist()
+    model.build(x_shape)
+    predictions = model(x)
+    print("Defined Theano model graph.")
+
+    def evaluate():
+        # Evaluate the accuracy of the MNIST model on legitimate test examples
+        accuracy = th_model_eval(x, y, predictions, X_test, Y_test, args=args)
+        assert X_test.shape[0] == 10000, X_test.shape
+        print('Test accuracy on legitimate test examples: ' + str(accuracy))
+        pass
+
+    # Train an MNIST model
+    th_model_train(x, y, predictions, model.trainable_weights, X_train, Y_train, evaluate=evaluate, args=args)
+
+
+    # Craft adversarial examples using Fast Gradient Sign Method (FGSM)
+    adv_x = fgsm(x, predictions, eps=0.3)
+    X_test_adv, = batch_eval([x], [adv_x], [X_test], args=args)
+    assert X_test_adv.shape[0] == 10000, X_test_adv.shape
+
+    # Evaluate the accuracy of the MNIST model on adversarial examples
+    accuracy = th_model_eval(x, y, predictions, X_test_adv, Y_test, args=args)
+    print('Test accuracy on adversarial examples: ' + str(accuracy))
+
+    print("Repeating the process, using adversarial training")
+    # Redefine Theano model graph
+    model_2 = model_mnist()
+    model_2.build(x_shape)
+    predictions_2 = model_2(x)
+    adv_x_2 = fgsm(x, predictions_2, eps=0.3)
+    predictions_2_adv = model_2(adv_x_2)
+
+
+    def evaluate_2():
+        # Evaluate the accuracy of the adversarialy trained MNIST model on
+        # legitimate test examples
+        accuracy = th_model_eval(x, y, predictions_2, X_test, Y_test, args=args)
+        print('Test accuracy on legitimate test examples: ' + str(accuracy))
+
+        # Evaluate the accuracy of the adversarially trained MNIST model on
+        # adversarial examples
+        accuracy_adv = th_model_eval(x, y, predictions_2_adv, X_test, Y_test, args=args)
+        print('Test accuracy on adversarial examples: ' + str(accuracy_adv))
+
+    # Perform adversarial training
+    th_model_train(x, y, predictions_2, model_2.trainable_weights, X_train, Y_train, predictions_adv=predictions_2_adv,
+            evaluate=evaluate_2, args=args)
+
+
+
+if __name__ == '__main__':
+    main()
