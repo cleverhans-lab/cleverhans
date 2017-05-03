6cbe4e6a3a7687d3af558b2b975a797838c34bf2
npapernot <ngp5056@cse.psu.edu>
2017-05-03 17:47:48 +0200

    tests for Attack class

--- tests_th/test_attack_class.py
+++ tests_th/test_attack_class.py
@@ -10,13 +10,12 @@ from cleverhans.attacks import Attack
 
 class TestAttackClassInitArguments(unittest.TestCase):
     def test_model(self):
-        import tensorflow as tf
-        sess = tf.Session()
+        import theano.tensor as T
 
         # Exception is thrown when model does not have __call__ attribute
         with self.assertRaises(Exception) as context:
-            model = tf.placeholder(tf.float32, shape=(None, 10))
-            Attack(model, back='tf', sess=sess)
+            model = T.matrix('y')
+            Attack(model, back='th', sess=None)
         self.assertTrue(context.exception)
 
     def test_back(self):
@@ -34,9 +33,9 @@ class TestAttackClassInitArguments(unittest.TestCase):
         def model():
             return True
 
-        # Exception is thrown when no session provided with TF
+        # Exception is thrown when session provided with TH
         with self.assertRaises(Exception) as context:
-            Attack(model, back='tf', sess=None)
+            Attack(model, back='th', sess=1)
         self.assertTrue(context.exception)
 
 
@@ -45,16 +44,12 @@ class TestAttackGenerate(unittest.TestCase):
         def model(x):
             return x
 
-        import numpy as np
-        import tensorflow as tf
-        sess = tf.Session()
-        x = tf.placeholder(tf.float32, shape=(1,))
-
-        test_attack = Attack(model, back='tf', sess=sess)
-        adv_x = test_attack.generate(x)
+        import theano.tensor as T
+        x = T.tensor4('x')
+        test_attack = Attack(model, back='th', sess=None)
 
         with self.assertRaises(Exception) as context:
-            sess.run(adv_x, feed_dict={x: np.asarray(1.0).reshape((1,))})
+            test_attack.generate(x)
         self.assertTrue(context.exception)
 
 
@@ -64,11 +59,9 @@ class TestAttackGenerateNp(unittest.TestCase):
             return x
 
         import numpy as np
-        import tensorflow as tf
-        sess = tf.Session()
         x_val = np.zeros((10, 5, 5, 1))
 
-        test_attack = Attack(model, back='tf', sess=sess)
+        test_attack = Attack(model, back='th', sess=None)
         with self.assertRaises(Exception) as context:
             test_attack.generate_np(x_val)
         self.assertTrue(context.exception)
@@ -79,10 +72,7 @@ class TestParseParams(unittest.TestCase):
         def model():
             return True
 
-        import tensorflow as tf
-        sess = tf.Session()
-
-        test_attack = Attack(model, back='tf', sess=sess)
+        test_attack = Attack(model, back='th', sess=None)
         self.assertTrue(test_attack.parse_params({}))
 
 
