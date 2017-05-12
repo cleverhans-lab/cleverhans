039f47696338c2d14ac1dead2d88c236c56c4795
liwei <liweihfyz@hotmail.com>
2017-05-12 10:27:45 +0800

    model_eval bug fix, wrongly use of tf.argmax

--- cleverhans/utils_tf.py
+++ cleverhans/utils_tf.py
@@ -207,7 +207,8 @@ def model_eval(sess, x, y, model, X_test, Y_test, args=None):
     assert args.batch_size, "Batch size was not given in args dict"
 
     # Define accuracy symbolically
-    correct_preds = tf.equal(tf.argmax(y, axis=-1), tf.argmax(model, axis=-1))
+    correct_preds = tf.equal(tf.argmax(y, axis=tf.rank(y) - 1),
+                             tf.argmax(model, axis=tf.rank(model) - 1))
     acc_value = tf.reduce_mean(tf.to_float(correct_preds))
 
     # Init result var
