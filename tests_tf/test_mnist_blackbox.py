32779530914c5f725b62b67891dbc257cf6912e7
npapernot <ngp5056@cse.psu.edu>
2017-05-29 15:35:47 -0400

    wrap tests in seperate functions

--- tests_tf/test_mnist_blackbox.py
+++ tests_tf/test_mnist_blackbox.py
@@ -1,21 +1,21 @@
 from tutorials import mnist_blackbox
-from tensorflow.python.platform import flags
-
-FLAGS = flags.FLAGS
 
 
-if __name__ == '__main__':
-    # Reduce number of data augmentations for faster runtime
-    FLAGS.data_aug = 4
-
+def test_mnist_blackbox():
     # Run the MNIST tutorial on a dataset of reduced size
-    test_dataset_indices = {'train_start': 0,
-                            'train_end': 10000,
-                            'test_start': 0,
-                            'test_end': 1666}
-    report = mnist_blackbox.mnist_blackbox(**test_dataset_indices)
+    # Reduce number of data augmentations for faster runtime
+    mnist_blackbox_args = {'train_start': 0,
+                           'train_end': 10000,
+                           'test_start': 0,
+                           'test_end': 1666,
+                           'data_aug': 4}
+    report = mnist_blackbox.mnist_blackbox(**mnist_blackbox_args)
 
     # Check accuracy values contained in the AccuracyReport object
     assert report['bbox'] > 0.85
     assert report['sub'] > 0.55
     assert report['bbox_on_sub_adv_ex'] < 0.65
+
+
+if __name__ == '__main__':
+    test_mnist_blackbox()
--- tests_tf/test_mnist_tutorial_jsma.py
+++ tests_tf/test_mnist_tutorial_jsma.py
@@ -1,10 +1,7 @@
 from tutorials import mnist_tutorial_jsma
-from tensorflow.python.platform import flags
-
-FLAGS = flags.FLAGS
 
 
-if __name__ == '__main__':
+def test_mnist_tutorial_jsma():
     # Run the MNIST tutorial on a dataset of reduced size
     # and disable visualization.
     jsma_tutorial_fnargs = {'train_start': 0,
@@ -21,3 +18,7 @@ if __name__ == '__main__':
     # There is no adversarial training in the JSMA tutorial
     assert report.adv_train_clean_eval == 0.
     assert report.adv_train_adv_eval == 0.
+
+
+if __name__ == '__main__':
+    test_mnist_tutorial_jsma()
