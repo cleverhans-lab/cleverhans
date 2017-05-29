e7e490df3cfd3cce8662d49904d53a7ce0eca43d
Nicolas Papernot <papernot@google.com>
2017-05-29 16:22:33 -0700

    reduce mnist adv training size test

--- tests_tf/test_mnist_tutorial_tf.py
+++ tests_tf/test_mnist_tutorial_tf.py
@@ -7,16 +7,16 @@ class TestAttackClassInitArguments(unittest.TestCase):
 
         # Run the MNIST tutorial on a dataset of reduced size
         test_dataset_indices = {'train_start': 0,
-                                'train_end': 10000,
+                                'train_end': 5000,
                                 'test_start': 0,
-                                'test_end': 1666}
+                                'test_end': 333}
         report = mnist_tutorial_tf.mnist_tutorial(**test_dataset_indices)
 
         # Check accuracy values contained in the AccuracyReport object
         self.assertTrue(report.clean_train_clean_eval > 0.85)
         self.assertTrue(report.clean_train_adv_eval < 0.06)
-        self.assertTrue(report.adv_train_clean_eval > 0.85)
-        self.assertTrue(report.adv_train_adv_eval > 0.190)
+        self.assertTrue(report.adv_train_clean_eval > 0.8)
+        self.assertTrue(report.adv_train_adv_eval > 0.2)
 
 
 if __name__ == '__main__':
