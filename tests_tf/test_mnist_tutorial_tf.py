e920cc068db740345949ce97a156c9664d3cfcfa
Nicolas Papernot <papernot@google.com>
2017-05-29 15:44:23 -0700

    refactor tests for tutorials

--- tests_tf/test_mnist_tutorial_tf.py
+++ tests_tf/test_mnist_tutorial_tf.py
@@ -1,20 +1,23 @@
-from tutorials import mnist_tutorial_tf
+import unittest
 
 
-def test_mnist_tutorial_tf():
-    # Run the MNIST tutorial on a dataset of reduced size
-    test_dataset_indices = {'train_start': 0,
-                            'train_end': 10000,
-                            'test_start': 0,
-                            'test_end': 1666}
-    report = mnist_tutorial_tf.mnist_tutorial(**test_dataset_indices)
+class TestAttackClassInitArguments(unittest.TestCase):
+    def test_mnist_tutorial_tf(self):
+        from tutorials import mnist_tutorial_tf
 
-    # Check accuracy values contained in the AccuracyReport object
-    assert report.clean_train_clean_eval > 0.85
-    assert report.clean_train_adv_eval < 0.06
-    assert report.adv_train_clean_eval > 0.85
-    assert report.adv_train_adv_eval > 0.190
+        # Run the MNIST tutorial on a dataset of reduced size
+        test_dataset_indices = {'train_start': 0,
+                                'train_end': 10000,
+                                'test_start': 0,
+                                'test_end': 1666}
+        report = mnist_tutorial_tf.mnist_tutorial(**test_dataset_indices)
+
+        # Check accuracy values contained in the AccuracyReport object
+        self.assertTrue(report.clean_train_clean_eval > 0.85)
+        self.assertTrue(report.clean_train_adv_eval < 0.06)
+        self.assertTrue(report.adv_train_clean_eval > 0.85)
+        self.assertTrue(report.adv_train_adv_eval > 0.190)
 
 
 if __name__ == '__main__':
-    test_mnist_tutorial_tf()
+    unittest.main()
