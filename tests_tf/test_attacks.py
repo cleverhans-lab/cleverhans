2e1f0d2145272620b722f8dc825943ef9c2119e7
Alexander Matyasko <alexander.matyasko@gmail.com>
2017-06-01 21:26:10 +0800

    Add basic tests for virtual adversarial training attack

---
+++ tests_tf/test_attacks.py
@@ -0,0 +1,48 @@
+from __future__ import absolute_import, division, print_function
+
+import unittest
+import numpy as np
+
+from cleverhans.attacks import VirtualAdversarialMethod
+
+
+class TestVirtualAdversarialMethod(unittest.TestCase):
+    def setUp(self):
+        import tensorflow as tf
+        import tensorflow.contrib.slim as slim
+
+        def dummy_model(x):
+            net = slim.fully_connected(x, 600)
+            return slim.fully_connected(net, 10, activation_fn=None)
+
+        self.sess = tf.Session()
+        self.sess.as_default()
+        self.model = tf.make_template('dummy_model', dummy_model)
+        self.attack = VirtualAdversarialMethod(self.model, sess=self.sess)
+
+        # initialize model
+        with tf.name_scope('dummy_model'):
+            self.model(tf.placeholder(tf.float32, shape=(None, 1000)))
+        self.sess.run(tf.initialize_all_variables())
+
+    def test_parse_params(self):
+        self.attack.parse_params()
+        # test default values
+        self.assertEqual(self.attack.eps, 2.0)
+        self.assertEqual(self.attack.num_iterations, 1)
+        self.assertEqual(self.attack.xi, 1e-6)
+        self.assertEqual(self.attack.clip_min, None)
+        self.assertEqual(self.attack.clip_max, None)
+
+    def test_generate_np(self):
+        x_val = np.random.rand(100, 1000)
+        perturbation = self.attack.generate_np(x_val) - x_val
+        perturbation_norm = np.sqrt(np.sum(perturbation**2, axis=1))
+        # test perturbation norm
+        self.assertTrue(np.allclose(perturbation_norm, self.attack.eps))
+
+
+if __name__ == '__main__':
+    # t = TestVirtualAdversarialMethod("test_generate_np")
+    # t.test_generate_np()
+    unittest.main()
