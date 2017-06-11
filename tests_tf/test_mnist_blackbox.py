df4c7f4134ab32790fb1cf1c33d1dcbcd70093f0
Nicolas Papernot <papernot@google.com>
2017-06-10 19:09:46 -0700

    make bbox tutorial test faster

--- tests_tf/test_mnist_blackbox.py
+++ tests_tf/test_mnist_blackbox.py
@@ -10,14 +10,16 @@ class TestMNISTBlackboxF(unittest.TestCase):
         mnist_blackbox_args = {'train_start': 0,
                                'train_end': 5000,
                                'test_start': 0,
-                               'test_end': 733,
-                               'data_aug': 2,
-                               'holdout': 400}
+                               'test_end': 2000,
+                               'data_aug': 1,
+                               'holdout': 1000,
+                               'nb_epochs': 2,
+                               'nb_epochs_s': 6}
         report = mnist_blackbox.mnist_blackbox(**mnist_blackbox_args)
 
         # Check accuracy values contained in the AccuracyReport object
-        self.assertTrue(report['bbox'] > 0.8, report['bbox'])
-        self.assertTrue(report['sub'] > 0.47, report['sub'])
+        self.assertTrue(report['bbox'] > 0.7, report['bbox'])
+        self.assertTrue(report['sub'] > 0.3, report['sub'])
         self.assertTrue(report['bbox_on_sub_adv_ex'] < 0.7, report['bbox_on_sub_adv_ex'])
 
 if __name__ == '__main__':
