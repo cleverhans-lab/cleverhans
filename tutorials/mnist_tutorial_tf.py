b6c674c387cd7f0d9e98f33a072f2799013765b7
Nicholas Carlini <nicholas@carlini.com>
2017-07-09 11:23:46 -0700

    Fix mnist_tutorial_tf generate feed issue

--- tutorials/mnist_tutorial_tf.py
+++ tutorials/mnist_tutorial_tf.py
@@ -263,8 +263,7 @@ def mnist_tutorial(train_start=0, train_end=60000, test_start=0,
     # Initialize the Fast Gradient Sign Method (FGSM) attack object and graph
     wrap = KerasModelWrapper(model)
     fgsm = FastGradientMethod(wrap, sess=sess)
-    fgsm_params = {'eps': 0.3,
-                   'y': Y_test}
+    fgsm_params = {'eps': 0.3}
     
     adv_x = fgsm.generate(x, **fgsm_params)
     preds_adv = model.get_probs(adv_x)
