40f6f29fe91a7c8c7becac37ab6e6d04642cccc3
Ian Goodfellow <goodfellow.ian@gmail.com>
2017-07-09 07:41:23 -0700

    update tests for new tutorial names

--- tests_tf/test_mnist_tutorial_keras_tf.py
+++ tests_tf/test_mnist_tutorial_keras_tf.py
@@ -1,23 +1,26 @@
 import unittest
 
 
-class TestMNISTTutorialTF(unittest.TestCase):
-    def test_mnist_tutorial_tf(self):
-        from tutorials import mnist_tutorial_pure_tf
+class TestMNISTTutorialKerasTF(unittest.TestCase):
+    def test_mnist_tutorial_keras_tf(self):
+        from tutorials import mnist_tutorial_keras_tf
 
         # Run the MNIST tutorial on a dataset of reduced size
         test_dataset_indices = {'train_start': 0,
                                 'train_end': 5000,
                                 'test_start': 0,
                                 'test_end': 333,
-                                'nb_epochs': 2}
-        report = mnist_tutorial_pure_tf.mnist_tutorial(**test_dataset_indices)
+                                'nb_epochs': 3,
+                                'train_dir': '/tmp',
+                                'filename': 'mnist.ckpt',
+                                'load_model': False}
+        report = mnist_tutorial_keras_tf.mnist_tutorial(**test_dataset_indices)
 
         # Check accuracy values contained in the AccuracyReport object
         self.assertTrue(report.clean_train_clean_eval > 0.75)
         self.assertTrue(report.clean_train_adv_eval < 0.1)
         self.assertTrue(report.adv_train_clean_eval > 0.7)
-        self.assertTrue(report.adv_train_adv_eval > 0.07)
+        self.assertTrue(report.adv_train_adv_eval > 0.06)
 
 
 if __name__ == '__main__':
