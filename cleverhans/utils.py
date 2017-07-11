9711fd60ffc52ca7eb68229d22abd8e991a5cd1d
Nicolas Papernot <papernot@google.com>
2017-07-11 15:42:41 -0700

    fix missing blank line

--- cleverhans/utils.py
+++ cleverhans/utils.py
@@ -11,6 +11,7 @@ known_number_types = (int, float, np.float16, np.float32, np.float64,
                       np.int8, np.int16, np.int32, np.int32, np.int64,
                       np.uint8, np.uint16, np.uint32, np.uint64)
 
+
 class _ArgsWrapper(object):
 
     """
