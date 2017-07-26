13f3fc2e150c99111c793691e874bf2489d18490
Willi Gierke <willi.gierke@student.hpi.de>
2017-07-27 00:27:18 +0200

    revert download_images and add multiprocessed version without additional dependencies

--- examples/nips17_adversarial_competition/dataset/download_images_multiprocessed.py
+++ examples/nips17_adversarial_competition/dataset/download_images_multiprocessed.py
@@ -1,7 +1,7 @@
-"""Script which downloads dataset images.
+"""Script which downloads dataset images using multiple processes.
 
 Usage:
-  python download_images.py --input_file=INPUT_FILE --output_dir=IMAGES_DIR
+  python download_images_multiprocessed.py --input_file=INPUT_FILE --output_dir=IMAGES_DIR
 
 where:
   INPUT_FILE is input csv file with dataset description, i.e. dev_dataset.csv
@@ -10,8 +10,8 @@ where:
 Example:
   # create directory for images
   mkdir images
-  # download images declared in dev_dataset.csv
-  python download_images.py --input_file=dev_dataset.csv --output_dir=images
+  # download images declared in dev_dataset.csv using multiple processes
+  python download_images_multiprocessed.py --input_file=dev_dataset.csv --output_dir=images
 
 
 Dependencies:
@@ -24,9 +24,11 @@ from __future__ import division
 from __future__ import print_function
 
 import argparse
-import csv
 import os
 import sys
+import csv
+from multiprocessing.dummy import Pool as ThreadPool
+from functools import partial
 
 from PIL import Image
 from io import BytesIO
@@ -65,7 +67,8 @@ def download_image(image_id, url, x1, y1, x2, y2, output_dir):
         image = Image.open(BytesIO(image_buffer)).convert('RGB')
         w = image.size[0]
         h = image.size[1]
-        image = image.crop((int(x1 * w), int(y1 * h), int(x2 * w), int(y2 * h)))
+        image = image.crop(
+            (int(x1 * w), int(y1 * h), int(x2 * w), int(y2 * h)))
         image = image.resize((299, 299), resample=Image.ANTIALIAS)
         image.save(output_filename)
     except IOError:
@@ -73,47 +76,33 @@ def download_image(image_id, url, x1, y1, x2, y2, output_dir):
     return True
 
 
+def get_images(row, output_dir):
+    if not download_image(image_id=row[0],
+                          url=row[1],
+                          x1=float(row[2]),
+                          y1=float(row[3]),
+                          x2=float(row[4]),
+                          y2=float(row[5]),
+                          output_dir=output_dir):
+        print("Download failed: " + str(row[0]))
+
+
 def main():
     input_filename, output_dir = parse_args()
 
-    if not os.path.isdir(os.getcwd() + '/' + output_dir):
+    if not os.path.isdir(os.path.join(os.getcwd(), output_dir)):
         print("Output directory {} does not exist".format(output_dir))
         sys.exit()
 
-    failed_to_download = set()
-    with open(input_filename) as input_file:
-        reader = csv.reader(input_file)
-        header_row = next(reader)
-        try:
-            row_idx_image_id = header_row.index('ImageId')
-            row_idx_url = header_row.index('URL')
-            row_idx_x1 = header_row.index('x1')
-            row_idx_y1 = header_row.index('y1')
-            row_idx_x2 = header_row.index('x2')
-            row_idx_y2 = header_row.index('y2')
-        except ValueError as e:
-            print('One of the columns was not found in the source file: ', e.message)
-
-        for idx, row in enumerate(reader):
-            if len(row) < len(header_row):
-                # skip partial or empty lines
-                continue
-            if not download_image(image_id=row[row_idx_image_id],
-                                  url=row[row_idx_url],
-                                  x1=float(row[row_idx_x1]),
-                                  y1=float(row[row_idx_y1]),
-                                  x2=float(row[row_idx_x2]),
-                                  y2=float(row[row_idx_y2]),
-                                  output_dir=output_dir):
-                failed_to_download.add(row[row_idx_image_id])
-            sys.stdout.write('\rDownloaded {0} images'.format(idx + 1))
-            sys.stdout.flush()
-
-        print()
-        if failed_to_download:
-            print('\nUnable to download images with the following IDs:')
-            for image_id in failed_to_download:
-                print(image_id)
+    with open(input_filename, 'r') as csv_file:
+        reader = csv.reader(csv_file)
+        rows = list(reader)[1:]
+    pool = ThreadPool(8)
+    partial_get_images = partial(get_images, output_dir=output_dir)
+    for i, _ in enumerate(pool.imap_unordered(partial_get_images, rows), 1):
+        sys.stderr.write('\rDownloaded {0} images'.format(i + 1))
+    pool.close()
+    pool.join()
 
 
 if __name__ == '__main__':
