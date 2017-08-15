d017308687e4d0414509160660fde2882b7343b9
Nicholas Carlini <nicholas@carlini.com>
2017-08-15 07:46:42 -0700

    Report distance on successes only

--- cleverhans/attacks_tf.py
+++ cleverhans/attacks_tf.py
@@ -705,8 +705,8 @@ class CarliniWagnerL2(object):
             _logger.debug("  Successfully generated adversarial examples " +
                           "on {} of {} instances.".
                           format(sum(upper_bound < 1e9), batch_size))
-            mean = np.mean(np.sqrt(o_bestl2))
-            _logger.debug("   Mean distortion: {:.4g}".format(mean))
+            mean = np.mean(np.sqrt(o_bestl2[o_bestl2<1e9]))
+            _logger.debug("   Mean successful distortion: {:.4g}".format(mean))
 
         # return the best solution found
         o_bestl2 = np.array(o_bestl2)
