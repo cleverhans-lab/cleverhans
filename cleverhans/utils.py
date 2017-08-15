ab041abaf619986ea314bfff24569837f81306e9
Ian Goodfellow <goodfellow@google.com>
2017-08-14 18:50:47 -0700

    factor out some of the dictionary determinism code

--- cleverhans/utils.py
+++ cleverhans/utils.py
@@ -259,3 +259,13 @@ def create_logger(name):
         base.addHandler(ch)
 
     return base
+
+
+def deterministic_dict(normal_dict):
+    """
+    Returns a version of `normal_dict` whose iteration order is always the same
+    """
+    out = OrderedDict()
+    for key in sorted(normal_dict.keys()):
+        out[key] = normal_dict[key]
+    return out
