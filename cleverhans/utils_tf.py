b93dfc457c18db3c87717b19997d4e9d28beb461
npapernot <ngp5056@cse.psu.edu>
2017-08-16 10:00:18 -0700

    remove deprecated tf_model_train

--- cleverhans/utils_tf.py
+++ cleverhans/utils_tf.py
@@ -88,12 +88,6 @@ def initialize_uninitialized_global_variables(sess):
         sess.run(tf.variables_initializer(not_initialized_vars))
 
 
-def tf_model_train(*args, **kwargs):
-    warnings.warn("`tf_model_train` is deprecated. Switch to `model_train`."
-                  "`tf_model_train` will be removed after 2017-07-18.")
-    return model_train(*args, **kwargs)
-
-
 def model_train(sess, x, y, predictions, X_train, Y_train, save=False,
                 predictions_adv=None, init_all=True, evaluate=None,
                 verbose=True, feed=None, args=None):
