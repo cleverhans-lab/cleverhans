59826e05ea33463b98e573c52607000459373872
fartash <fartash.faghri@gmail.com>
2017-10-09 15:44:24 -0400

    removed fast step feature adversaries. Simplified FFA attack.

--- cleverhans/feat_advs.py
+++ cleverhans/feat_advs.py
@@ -12,133 +12,7 @@ from cleverhans.attacks import Attack
 from cleverhans.model import Model
 
 
-def ffg(x, s_feat, g_feat, eps=0.3, ord=np.inf, clip_min=None, clip_max=None):
-    """
-    TensorFlow implementation of the Fast Feature Gradient. This is a
-    single step attack similar to Fast Gradient Method that attacks an
-    internal representation.
-
-    :param x: the input placeholder
-    :param s_feat: model's internal tensor for source
-    :param g_feat: model's internal tensor for guide
-    :param eps: the epsilon (input variation parameter)
-    :param ord: (optional) Order of the norm (mimics NumPy).
-                Possible values: np.inf, 1 or 2.
-    :param clip_min: Minimum float value for adversarial example components
-    :param clip_max: Maximum float value for adversarial example components
-    :return: a tensor for the adversarial example
-    """
-
-    # feat.shape = (batch, c) or (batch, w, h, c)
-    axis = range(1, len(s_feat.shape))
-
-    # Compute loss
-    loss = tf.reduce_sum(tf.square(s_feat - g_feat), axis)
-
-    # Define gradient of loss wrt input
-    grad, = tf.gradients(loss, x)
-
-    if ord == np.inf:
-        # Take sign of gradient
-        signed_grad = tf.sign(grad)
-    elif ord == 1:
-        reduc_ind = list(xrange(1, len(x.get_shape())))
-        signed_grad = grad / tf.reduce_sum(tf.abs(grad),
-                                           reduction_indices=reduc_ind,
-                                           keep_dims=True)
-    elif ord == 2:
-        reduc_ind = list(xrange(1, len(x.get_shape())))
-        signed_grad = grad / tf.sqrt(tf.reduce_sum(tf.square(grad),
-                                                   reduction_indices=reduc_ind,
-                                                   keep_dims=True))
-    else:
-        raise NotImplementedError("Only L-inf, L1 and L2 norms are "
-                                  "currently implemented.")
-
-    # Multiply by constant epsilon
-    scaled_signed_grad = eps * signed_grad
-
-    # Add perturbation to original example to obtain adversarial example
-    adv_x = tf.stop_gradient(x + scaled_signed_grad)
-
-    # If clipping is needed, reset all values outside of [clip_min, clip_max]
-    if (clip_min is not None) and (clip_max is not None):
-        adv_x = tf.clip_by_value(adv_x, clip_min, clip_max)
-
-    return adv_x
-
-
-class FastFeatureGradient(Attack):
-
-    """
-    This is similar to FGSM but done for the internal representations.
-    """
-
-    def __init__(self, model, back='tf', sess=None):
-        """
-        Create a FastFeatureGradient instance.
-        """
-        super(FastFeatureGradient, self).__init__(model, back, sess)
-        self.feedable_kwargs = {'eps': np.float32,
-                                'layer': str,
-                                'clip_min': np.float32,
-                                'clip_max': np.float32}
-        self.structural_kwargs = ['ord']
-
-    def generate(self, x, g, **kwargs):
-        """
-        Generate symbolic graph for adversarial examples and return.
-        :param x: The model's symbolic inputs for a source.
-        :param g: The model's symoblic input for a guide.
-        :param eps: (optional float) attack step size (input variation)
-        :param ord: (optional) Order of the norm (mimics NumPy).
-                    Possible values: np.inf, 1 or 2.
-        :param clip_min: (optional float) Minimum input component value
-        :param clip_max: (optional float) Maximum input component value
-        """
-        # Parse and save attack-specific parameters
-        assert self.parse_params(**kwargs)
-
-        s_feat = self.model.get_layer(x, self.layer)
-        g_feat = self.model.get_layer(g, self.layer)
-        return ffg(x, s_feat, g_feat, eps=self.eps,
-                   ord=self.ord, clip_min=self.clip_min,
-                   clip_max=self.clip_max)
-
-    def parse_params(self, layer=None, eps=0.3, ord=np.inf,
-                     clip_min=None, clip_max=None, **kwargs):
-        """
-        Take in a dictionary of parameters and applies attack-specific checks
-        before saving them as attributes.
-
-        Attack-specific parameters:
-        :param layer: The name of the layer to be manipulated by the attack
-        :param eps: (optional float) attack step size (input variation)
-        :param ord: (optional) Order of the norm (mimics NumPy).
-                    Possible values: np.inf, 1 or 2.
-        :param clip_min: (optional float) Minimum input component value
-        :param clip_max: (optional float) Maximum input component value
-        """
-        # Save attack-specific parameters
-
-        self.layer = layer
-        self.eps = eps
-        self.ord = ord
-        self.clip_min = clip_min
-        self.clip_max = clip_max
-
-        # Check if order of the norm is acceptable given current implementation
-        if self.layer not in self.model.get_layer_names():
-            raise ValueError("Layer name does not exist in the model layers.")
-        if self.ord not in [np.inf, int(1), int(2)]:
-            raise ValueError("Norm order must be either np.inf, 1, or 2.")
-        if self.back == 'th' and self.ord != np.inf:
-            raise NotImplementedError("The only FastGradientMethod norm "
-                                      "implemented for Theano is np.inf.")
-        return True
-
-
-class FastIterativeFeatureAdversaries(Attack):
+class FastFeatureAdversaries(Attack):
 
     """
     This is similar to Basic Iterative Method (Kurakin et al. 2016) but
@@ -147,10 +21,9 @@ class FastIterativeFeatureAdversaries(Attack):
 
     def __init__(self, model, back='tf', sess=None):
         """
-        Create a FeatureAdversaries instance.
+        Create a FastFeatureAdversaries instance.
         """
-        super(FastIterativeFeatureAdversaries, self).__init__(model, back,
-                                                              sess)
+        super(FastFeatureAdversaries, self).__init__(model, back, sess)
         self.feedable_kwargs = {'eps': np.float32,
                                 'eps_iter': np.float32,
                                 'clip_min': np.float32,
@@ -159,53 +32,6 @@ class FastIterativeFeatureAdversaries(Attack):
         self.structural_kwargs = ['ord', 'nb_iter']
 
         assert isinstance(self.model, Model)
-
-    def generate(self, x, g, **kwargs):
-        """
-        Generate symbolic graph for adversarial examples and return.
-        :param x: The model's symbolic inputs.
-        :param g: The target's symbolic representation.
-        :param eps: (required float) maximum distortion of adversarial example
-                    compared to original input
-        :param eps_iter: (required float) step size for each attack iteration
-        :param nb_iter: (required int) Number of attack iterations.
-        :param y: (optional) A tensor with the model labels.
-        :param y_target: (optional) A tensor with the labels to target. Leave
-                         y_target=None if y is also set. Labels should be
-                         one-hot-encoded.
-        :param ord: (optional) Order of the norm (mimics Numpy).
-                    Possible values: np.inf, 1 or 2.
-        :param clip_min: (optional float) Minimum input component value
-        :param clip_max: (optional float) Maximum input component value
-        """
-        import tensorflow as tf
-        from utils_tf import clip_eta
-
-        # Parse and save attack-specific parameters
-        assert self.parse_params(**kwargs)
-
-        # Initialize loop variables
-        eta = tf.truncated_normal(tf.shape(x), self.eps_iter / 2)
-        eta = clip_eta(eta, self.ord, self.eps)
-
-        fgm_params = {'eps': self.eps_iter, 'layer': self.layer,
-                      'ord': self.ord, 'clip_min': self.clip_min,
-                      'clip_max': self.clip_max}
-
-        for i in range(self.nb_iter):
-            FFG = FastFeatureGradient(self.model, back=self.back,
-                                      sess=self.sess)
-            # Compute this step's perturbation
-            eta = FFG.generate(x + eta, g, **fgm_params) - x
-
-            eta = clip_eta(eta, self.ord, self.eps)
-
-        # Define adversarial example (and clip if necessary)
-        adv_x = x + eta
-        if self.clip_min is not None and self.clip_max is not None:
-            adv_x = tf.clip_by_value(adv_x, self.clip_min, self.clip_max)
-
-        return adv_x
 
     def parse_params(self, layer=None, eps=0.3, eps_iter=0.05, nb_iter=10,
                      ord=np.inf, clip_min=None, clip_max=None, **kwargs):
@@ -247,3 +73,81 @@ class FastIterativeFeatureAdversaries(Attack):
             raise NotImplementedError(error_string)
 
         return True
+
+    def attack_single_step(self, x, eta, s_feat, g_feat):
+        """
+        TensorFlow implementation of the Fast Feature Gradient. This is a
+        single step attack similar to Fast Gradient Method that attacks an
+        internal representation.
+
+        :param x: the input placeholder
+        :param eta: A tensor the same shape as x that holds the perturbation.
+        :param s_feat: model's internal tensor for source
+        :param g_feat: model's internal tensor for guide
+        :return: a tensor for the adversarial example
+        """
+        from utils_tf import clip_eta
+
+        adv_x = x + eta
+
+        # feat.shape = (batch, c) or (batch, w, h, c)
+        axis = range(1, len(s_feat.shape))
+
+        # Compute loss
+        loss = tf.reduce_sum(tf.square(s_feat - g_feat), axis)
+
+        # Define gradient of loss wrt input
+        grad, = tf.gradients(loss, x)
+
+        # Multiply by constant epsilon
+        scaled_signed_grad = self.eps_iter * tf.sign(grad)
+
+        # Add perturbation to original example to obtain adversarial example
+        adv_x = adv_x + scaled_signed_grad
+
+        # If clipping is needed,
+        # reset all values outside of [clip_min, clip_max]
+        if (self.clip_min is not None) and (self.clip_max is not None):
+            adv_x = tf.clip_by_value(adv_x, self.clip_min, self.clip_max)
+
+        eta = adv_x - x
+        eta = clip_eta(eta, self.ord, self.eps)
+
+        return eta
+
+    def generate(self, x, g, **kwargs):
+        """
+        Generate symbolic graph for adversarial examples and return.
+        :param x: The model's symbolic inputs.
+        :param g: The target's symbolic representation.
+        :param eps: (required float) maximum distortion of adversarial example
+                    compared to original input
+        :param eps_iter: (required float) step size for each attack iteration
+        :param nb_iter: (required int) Number of attack iterations.
+        :param ord: (optional) Order of the norm (mimics Numpy).
+                    Possible values: np.inf, 1 or 2.
+        :param clip_min: (optional float) Minimum input component value
+        :param clip_max: (optional float) Maximum input component value
+        """
+        import tensorflow as tf
+        from utils_tf import clip_eta
+
+        # Parse and save attack-specific parameters
+        assert self.parse_params(**kwargs)
+
+        s_feat = self.model.get_layer(x, self.layer)
+        g_feat = self.model.get_layer(g, self.layer)
+
+        # Initialize loop variables
+        eta = tf.random_uniform(tf.shape(x), -self.eps, self.eps)
+        eta = clip_eta(eta, self.ord, self.eps)
+
+        for i in range(self.nb_iter):
+            eta = self.attack_single_step(x, eta, s_feat, g_feat)
+
+        # Define adversarial example (and clip if necessary)
+        adv_x = x + eta
+        if self.clip_min is not None and self.clip_max is not None:
+            adv_x = tf.clip_by_value(adv_x, self.clip_min, self.clip_max)
+
+        return adv_x
