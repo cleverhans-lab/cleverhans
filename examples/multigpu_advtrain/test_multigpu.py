b054f9bf271119ed46f4433b8d698e0a6b2c80d8
Fartash Faghri <fartash.faghri@gmail.com>
2017-11-03 20:19:06 -0400

    test added for run_multigpu madryetal

--- examples/multigpu_advtrain/test_multigpu.py
+++ examples/multigpu_advtrain/test_multigpu.py
@@ -10,96 +10,117 @@ from collections import namedtuple
 
 from cleverhans.utils import AccuracyReport
 from cleverhans.devtools.checks import CleverHansTest
-from cleverhans.attacks import MadryEtAl
+from attacks_multigpu import MadryEtAlMultiGPU
+
+import sys
+import os
+sys.path.insert(0, os.path.abspath('../../tests_tf/'))
+from test_attacks import TestMadryEtAl  # NOQA
 
 
-class TestMadryEtAl(CleverHansTest):
+class TestMadryEtAlMultiGPU(TestMadryEtAl):
     def setUp(self):
-        super(TestMadryEtAl, self).setUp()
+        super(TestMadryEtAlMultiGPU, self).setUp()
+        self.attack_single_gpu = self.attack
+        self.attack_multi_gpu = MadryEtAlMultiGPU(self.model, sess=self.sess,
+                                                  ngpu=2)
+        self.attack = self.attack_multi_gpu
+
+    def test_single_vs_multi_gpu(self):
+        x_val = np.random.rand(100, 2)
+        x_val = np.array(x_val, dtype=np.float32)
+
+        def multi_attack(attack):
+            flags = {'eps': 1.0, 'eps_iter': 0.05,
+                     'clip_min': 0.5, 'clip_max': 0.7, 'nb_iter': 5}
+
+            orig_labs = np.argmax(self.sess.run(self.model(x_val)), axis=1)
+            new_labs_multi = orig_labs.copy()
+            # Generate multiple adversarial examples
+            for i in range(10):
+                x_adv = attack.generate_np(x_val, **flags)
+                new_labs = np.argmax(self.sess.run(self.model(x_adv)), axis=1)
+
+                # Examples for which we have not found adversarial examples
+                I = (orig_labs == new_labs_multi)
+                new_labs_multi[I] = new_labs[I]
+
+            return np.mean(orig_labs == new_labs_multi)
+
+        acc_s = multi_attack(self.attack_single_gpu)
+        acc_m = multi_attack(self.attack_multi_gpu)
+
+        self.assertClose(acc_s, acc_m, atol=5e-3)
+
+
+class TestRunMultiGPU(CleverHansTest):
+    def test_run_multi_gpu_madryetal(self):
+        np.random.seed(42)
         import tensorflow as tf
+        tf.set_random_seed(42)
 
-        # The world's simplest neural network
-        def my_model(x):
-            W1 = tf.constant([[1.5, .3], [-2, 0.3]], dtype=tf.float32)
-            h1 = tf.nn.sigmoid(tf.matmul(x, W1))
-            W2 = tf.constant([[-2.4, 1.2], [0.5, -2.3]], dtype=tf.float32)
-            res = tf.matmul(h1, W2)
-            return res
+        from run_multigpu import run_trainer
 
-        self.sess = tf.Session()
-        self.model = my_model
-        self.attack = MadryEtAl(self.model, sess=self.sess)
+        # Run the trainers on a dataset of reduced size
+        flags = {'train_start': 0,
+                 'train_end': 5000,
+                 'test_start': 0,
+                 'test_end': 333,
+                 'nb_epochs': 2,
+                 'testing': True}
 
-    def test_attack_strength(self):
-        """
-        If clipping is not done at each iteration (not using clip_min and
-        clip_max), this attack fails by
-        np.mean(orig_labels == new_labels) == .5
-        """
-        x_val = np.random.rand(100, 2)
-        x_val = np.array(x_val, dtype=np.float32)
+        # Run the multi-gpu trainer for adversarial training
+        flags.update({'batch_size': 128, 'learning_rate': 0.001,
+                      'dataset': 'mnist', 'only_adv_train': False,
+                      'eval_iters': 1, 'fast_tests': True,
+                      'debug_graph': False,
+                      'save_dir': None, 'save_steps': 10000,
+                      'attack_nb_iter_train': 2, 'sync_step': None,
+                      })
 
-        x_adv = self.attack.generate_np(x_val, eps=1.0, eps_iter=0.05,
-                                        clip_min=0.5, clip_max=0.7,
-                                        nb_iter=5)
+        flags.update({'adv_train': True,
+                      'save': False,
+                      'model_type': 'basic',
+                      'ngpu': 1,
+                      'attack_type_test': 'MadryEtAl_y',
+                      'attack_type_train': 'MadryEtAl_y',
+                      })
+        HParams = namedtuple('HParams', flags.keys())
 
-        orig_labs = np.argmax(self.sess.run(self.model(x_val)), axis=1)
-        new_labs = np.argmax(self.sess.run(self.model(x_adv)), axis=1)
-        print(np.mean(orig_labs == new_labs))
-        self.assertTrue(np.mean(orig_labs == new_labs) < 0.1)
+        hparams = HParams(**flags)
+        report_dict = run_trainer(hparams)
+        report_s = AccuracyReport()
+        report_s.train_adv_train_clean_eval = report_dict['train']
+        report_s.adv_train_clean_eval = report_dict['test']
+        report_s.adv_train_adv_eval = report_dict['MadryEtAl_y']
 
-    def test_clip_eta(self):
-        x_val = np.random.rand(100, 2)
-        x_val = np.array(x_val, dtype=np.float32)
+        # Run the multi-gpu trainer for adversarial training using 2 gpus
+        # trainer_multigpu by default sets `allow_soft_placement=True`
+        flags.update({'ngpu': 2,
+                      'attack_type_train': 'MadryEtAl_y_multigpu',
+                      'sync_step': 1})
+        hparams = HParams(**flags)
+        report_dict = run_trainer(hparams)
+        report_m = AccuracyReport()
+        report_m.train_adv_train_clean_eval = report_dict['train']
+        report_m.adv_train_clean_eval = report_dict['test']
+        report_m.adv_train_adv_eval = report_dict['MadryEtAl_y']
 
-        x_adv = self.attack.generate_np(x_val, eps=1.0, eps_iter=0.1,
-                                        nb_iter=5)
+        # Check that the tutorial is deterministic (seeded properly)
+        if LooseVersion(tf.__version__) >= LooseVersion('1.1.0'):
+            atol_fac = 3
+        else:
+            atol_fac = 2
 
-        delta = np.max(np.abs(x_adv - x_val), axis=1)
-        self.assertTrue(np.all(delta <= 1.))
-
-    def test_generate_np_gives_clipped_adversarial_examples(self):
-        x_val = np.random.rand(100, 2)
-        x_val = np.array(x_val, dtype=np.float32)
-
-        x_adv = self.attack.generate_np(x_val, eps=1.0, eps_iter=0.1,
-                                        nb_iter=5,
-                                        clip_min=-0.2, clip_max=0.3)
-
-        self.assertTrue(-0.201 < np.min(x_adv))
-        self.assertTrue(np.max(x_adv) < .301)
-
-    def test_multiple_initial_random_step(self):
-        """
-        This test generates multiple adversarial examples until an adversarial
-        example is generated with a different label compared to the original
-        label. This is the procedure suggested in Madry et al. (2017).
-
-        This test will fail if an initial random step is not taken (error>0.5).
-        """
-        x_val = np.random.rand(100, 2)
-        x_val = np.array(x_val, dtype=np.float32)
-
-        orig_labs = np.argmax(self.sess.run(self.model(x_val)), axis=1)
-        new_labs_multi = orig_labs.copy()
-
-        # Generate multiple adversarial examples
-        for i in range(10):
-            x_adv = self.attack.generate_np(x_val, eps=.5, eps_iter=0.05,
-                                            clip_min=0.5, clip_max=0.7,
-                                            nb_iter=2)
-            new_labs = np.argmax(self.sess.run(self.model(x_adv)), axis=1)
-
-            # Examples for which we have not found adversarial examples
-            I = (orig_labs == new_labs_multi)
-            new_labs_multi[I] = new_labs[I]
-
-        self.assertTrue(np.mean(orig_labs == new_labs_multi) < 0.1)
-
-
-class TestMultiGPUAdvTrain(CleverHansTest):
-    def test_run_multigpu_madryetal(self):
-        pass
+        self.assertClose(report_s.train_adv_train_clean_eval,
+                         report_m.train_adv_train_clean_eval,
+                         atol=atol_fac * 1e-2)
+        self.assertClose(report_s.adv_train_clean_eval,
+                         report_m.adv_train_clean_eval,
+                         atol=atol_fac * 5e-3)
+        self.assertClose(report_s.adv_train_adv_eval,
+                         report_m.adv_train_adv_eval,
+                         atol=atol_fac * 5e-3)
 
     def test_run_single_gpu_fgsm(self):
 
@@ -119,25 +140,16 @@ class TestMultiGPUAdvTrain(CleverHansTest):
                  'testing': True}
         report = mnist_tutorial_tf.mnist_tutorial(**flags)
 
-        # Check that the tutorial is deterministic (seeded properly)
-        if LooseVersion(tf.__version__) >= LooseVersion('1.1.0'):
-            atol_fac = 4
-        else:
-            atol_fac = 2
-
         # Run the multi-gpu trainer for clean training
         flags.update({'batch_size': 128, 'learning_rate': 0.001,
                       'dataset': 'mnist', 'only_adv_train': False,
                       'eval_iters': 1, 'ngpu': 1, 'fast_tests': False,
                       'debug_graph': False, 'attack_type_train': None,
                       'save_dir': None, 'save_steps': 10000,
-                      'attack_nb_iter_train': None})
+                      'attack_nb_iter_train': None, 'save': False,
+                      'model_type': 'basic', 'attack_type_test': 'FGSM'})
 
-        flags.update({'adv_train': False,
-                      'save': False,
-                      'model_type': 'basic',
-                      'attack_type_test': 'FGSM',
-                      })
+        flags.update({'adv_train': False})
         HParams = namedtuple('HParams', flags.keys())
 
         hparams = HParams(**flags)
@@ -149,9 +161,6 @@ class TestMultiGPUAdvTrain(CleverHansTest):
 
         # Run the multi-gpu trainer for adversarial training
         flags.update({'adv_train': True,
-                      'save': False,
-                      'model_type': 'basic',
-                      'attack_type_test': 'FGSM',
                       'attack_type_train': 'FGSM',
                       })
         HParams = namedtuple('HParams', flags.keys())
@@ -161,6 +170,12 @@ class TestMultiGPUAdvTrain(CleverHansTest):
         report_2.train_adv_train_clean_eval = report_dict['train']
         report_2.adv_train_clean_eval = report_dict['test']
         report_2.adv_train_adv_eval = report_dict['FGSM']
+
+        # Check that the tutorial is deterministic (seeded properly)
+        if LooseVersion(tf.__version__) >= LooseVersion('1.1.0'):
+            atol_fac = 4
+        else:
+            atol_fac = 2
 
         self.assertClose(report.train_clean_train_clean_eval,
                          report_2.train_clean_train_clean_eval,
@@ -176,10 +191,10 @@ class TestMultiGPUAdvTrain(CleverHansTest):
                          atol=atol_fac * 2e-2)
         self.assertClose(report.adv_train_clean_eval,
                          report_2.adv_train_clean_eval,
-                         atol=atol_fac * 2e-1)
+                         atol=atol_fac * 2e-2)
         self.assertClose(report.adv_train_adv_eval,
                          report_2.adv_train_adv_eval,
-                         atol=atol_fac * 2e-1)
+                         atol=atol_fac * 2e-2)
 
 
 if __name__ == '__main__':
