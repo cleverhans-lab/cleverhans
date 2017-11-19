aec43cb7d395a8a3fb3117d0d3e39709980b4be9
Fartash Faghri <fartash.faghri@gmail.com>
2017-11-19 17:35:28 -0500

    fixed tests for run_multigpu, tests pass.

--- examples/multigpu_advtrain/test_run_multigpu.py
+++ examples/multigpu_advtrain/test_run_multigpu.py
@@ -2,30 +2,25 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
-from distutils.version import LooseVersion
 
 import unittest
 import numpy as np
+import tensorflow as tf
 from collections import namedtuple
 
 from cleverhans.utils import AccuracyReport
 from cleverhans.devtools.checks import CleverHansTest
+from run_multigpu import run_trainer
 
 
 class TestRunMultiGPU(CleverHansTest):
-    def test_run_multi_gpu_madryetal(self):
-        np.random.seed(42)
-        import tensorflow as tf
-        tf.set_random_seed(42)
-
-        from run_multigpu import run_trainer
-
+    def helper_run_multi_gpu_madryetal(self, extra_flags=None):
         # Run the trainers on a dataset of reduced size
         flags = {'train_start': 0,
                  'train_end': 5000,
                  'test_start': 0,
                  'test_end': 333,
-                 'nb_epochs': 2,
+                 'nb_epochs': 5,
                  'testing': True}
 
         # Run the multi-gpu trainer for adversarial training
@@ -34,68 +29,62 @@ class TestRunMultiGPU(CleverHansTest):
                       'eval_iters': 1, 'fast_tests': True,
                       'debug_graph': False,
                       'save_dir': None, 'save_steps': 10000,
-                      'attack_nb_iter_train': 40, 'sync_step': None,
-                      })
-
-        flags.update({'adv_train': True,
+                      'attack_nb_iter_train': 10, 'sync_step': None,
+                      'adv_train': True,
                       'save': False,
                       'model_type': 'basic',
-                      'ngpu': 1,
                       'attack_type_test': 'MadryEtAl_y',
-                      'attack_type_train': 'MadryEtAl_y',
                       })
-        HParams = namedtuple('HParams', flags.keys())
-
-        hparams = HParams(**flags)
-        report_dict = run_trainer(hparams)
-        report_s = AccuracyReport()
-        report_s.train_adv_train_clean_eval = report_dict['train']
-        report_s.adv_train_clean_eval = report_dict['test']
-        report_s.adv_train_adv_eval = report_dict['MadryEtAl_y']
+        if extra_flags is not None:
+            flags.update(extra_flags)
 
         # Run the multi-gpu trainer for adversarial training using 2 gpus
         # trainer_multigpu by default sets `allow_soft_placement=True`
         flags.update({'ngpu': 2,
                       'attack_type_train': 'MadryEtAl_y_multigpu',
                       'sync_step': 1})
+        HParams = namedtuple('HParams', flags.keys())
+
         hparams = HParams(**flags)
+        np.random.seed(42)
+        tf.set_random_seed(42)
         report_dict = run_trainer(hparams)
         report_m = AccuracyReport()
         report_m.train_adv_train_clean_eval = report_dict['train']
         report_m.adv_train_clean_eval = report_dict['test']
         report_m.adv_train_adv_eval = report_dict['MadryEtAl_y']
 
-        # Check that the tutorial is deterministic (seeded properly)
-        if LooseVersion(tf.__version__) >= LooseVersion('1.1.0'):
-            atol_fac = 3
-        else:
-            atol_fac = 2
+        flags.update({'ngpu': 1,
+                      'attack_type_train': 'MadryEtAl_y',
+                      })
+        hparams = HParams(**flags)
+        np.random.seed(42)
+        tf.set_random_seed(42)
+        report_dict = run_trainer(hparams)
+        report_s = AccuracyReport()
+        report_s.train_adv_train_clean_eval = report_dict['train']
+        report_s.adv_train_clean_eval = report_dict['test']
+        report_s.adv_train_adv_eval = report_dict['MadryEtAl_y']
 
         self.assertClose(report_s.train_adv_train_clean_eval,
                          report_m.train_adv_train_clean_eval,
-                         atol=atol_fac * 1e-2)
+                         atol=3e-2)
         self.assertClose(report_s.adv_train_clean_eval,
                          report_m.adv_train_clean_eval,
-                         atol=atol_fac * 5e-3)
+                         atol=2e-2)
         self.assertClose(report_s.adv_train_adv_eval,
                          report_m.adv_train_adv_eval,
-                         atol=atol_fac * 5e-3)
+                         atol=5e-2)
 
     def test_run_single_gpu_fgsm(self):
-
-        np.random.seed(42)
-        import tensorflow as tf
-        tf.set_random_seed(42)
-
         from cleverhans_tutorials import mnist_tutorial_tf
-        from run_multigpu import run_trainer
 
         # Run the MNIST tutorial on a dataset of reduced size
         flags = {'train_start': 0,
                  'train_end': 5000,
                  'test_start': 0,
                  'test_end': 333,
-                 'nb_epochs': 2,
+                 'nb_epochs': 5,
                  'testing': True}
         report = mnist_tutorial_tf.mnist_tutorial(**flags)
 
@@ -103,7 +92,7 @@ class TestRunMultiGPU(CleverHansTest):
         flags.update({'batch_size': 128, 'learning_rate': 0.001,
                       'dataset': 'mnist', 'only_adv_train': False,
                       'eval_iters': 1, 'ngpu': 1, 'fast_tests': False,
-                      'debug_graph': False, 'attack_type_train': None,
+                      'debug_graph': False, 'attack_type_train': '',
                       'save_dir': None, 'save_steps': 10000,
                       'attack_nb_iter_train': None, 'save': False,
                       'model_type': 'basic', 'attack_type_test': 'FGSM'})
@@ -112,6 +101,8 @@ class TestRunMultiGPU(CleverHansTest):
         HParams = namedtuple('HParams', flags.keys())
 
         hparams = HParams(**flags)
+        np.random.seed(42)
+        tf.set_random_seed(42)
         report_dict = run_trainer(hparams)
         report_2 = AccuracyReport()
         report_2.train_clean_train_clean_eval = report_dict['train']
@@ -125,35 +116,37 @@ class TestRunMultiGPU(CleverHansTest):
         HParams = namedtuple('HParams', flags.keys())
 
         hparams = HParams(**flags)
+        np.random.seed(42)
+        tf.set_random_seed(42)
         report_dict = run_trainer(hparams)
         report_2.train_adv_train_clean_eval = report_dict['train']
         report_2.adv_train_clean_eval = report_dict['test']
         report_2.adv_train_adv_eval = report_dict['FGSM']
 
-        # Check that the tutorial is deterministic (seeded properly)
-        if LooseVersion(tf.__version__) >= LooseVersion('1.1.0'):
-            atol_fac = 4
-        else:
-            atol_fac = 2
-
         self.assertClose(report.train_clean_train_clean_eval,
                          report_2.train_clean_train_clean_eval,
-                         atol=atol_fac * 5e-3)
+                         atol=3e-2)
         self.assertClose(report.clean_train_clean_eval,
                          report_2.clean_train_clean_eval,
-                         atol=atol_fac * 5e-3)
+                         atol=2e-2)
         self.assertClose(report.clean_train_adv_eval,
                          report_2.clean_train_adv_eval,
-                         atol=atol_fac * 5e-3)
+                         atol=2e-2)
         self.assertClose(report.train_adv_train_clean_eval,
                          report_2.train_adv_train_clean_eval,
-                         atol=atol_fac * 2e-2)
+                         atol=5e-2)
         self.assertClose(report.adv_train_clean_eval,
                          report_2.adv_train_clean_eval,
-                         atol=atol_fac * 2e-2)
+                         atol=5e-2)
         self.assertClose(report.adv_train_adv_eval,
                          report_2.adv_train_adv_eval,
-                         atol=atol_fac * 2e-2)
+                         atol=5e-2)
+
+    def test_run_multi_gpu_madryetal(self):
+        self.helper_run_multi_gpu_madryetal()
+
+    def test_run_multi_gpu_naive(self):
+        self.helper_run_multi_gpu_madryetal({'adv_train': False})
 
 
 if __name__ == '__main__':
