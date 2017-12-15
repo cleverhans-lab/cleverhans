5a1f6bfb04d5da2ca8b3f9fb75a8b8891acc503d
Ian Goodfellow <goodfellow@google.com>
2017-12-15 14:37:30 -0800

    Closes #324

---
+++ cleverhans/tests/test_utils_tf.py
@@ -0,0 +1,22 @@
+import numpy as np
+from cleverhans.utils_tf import clip_eta
+from cleverhans.devtools.checks import CleverHansTest
+import tensorflow as tf
+
+class TestUtils(CleverHansTest):
+    def setUp(self):
+        super(TestUtils, self).setUp()
+
+        self.sess = tf.Session()
+
+    def test_clip_eta_norm_0(self):
+        eta = tf.zeros((5, 3))
+        assert eta.dtype == tf.float32, eta.dtype
+        ord_arg = 2
+        eps = .25
+        for ord_arg in [np.inf, 1, 2]:
+            clipped = clip_eta(eta, ord_arg, eps)
+            clipped = self.sess.run(clipped)
+            assert not np.any(np.isinf(clipped))
+            assert not np.any(np.isnan(clipped)), (ord_arg, clipped)
+
