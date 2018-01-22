33114cf09f6661493ad17c9aa7f0d784eb856f4e
Aurko Roy <roy.aurko@gmail.com>
2018-01-22 22:51:17 +0000

    use thermoemter

--- cleverhans_tutorials/mnist_tutorial_lspga.py
+++ cleverhans_tutorials/mnist_tutorial_lspga.py
@@ -104,7 +104,7 @@ def mnist_tutorial(train_start=0, train_end=60000, test_start=0,
         'learning_rate': learning_rate
     }
     lspga_params = {'eps': 0.3, 'levels':16, 'steps':10, 'attack_step':1.0,
-                    'noisy_grads': False}
+                    'noisy_grads': False, thermometer: True}
     rng = np.random.RandomState([2017, 8, 30])
 
     if clean_train:
