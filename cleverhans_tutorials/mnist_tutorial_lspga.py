68f7f52b5f773e4818eff5cd4cbfa52c2e397f1f
Aurko Roy <roy.aurko@gmail.com>
2018-01-22 22:54:01 +0000

    minor param changes

--- cleverhans_tutorials/mnist_tutorial_lspga.py
+++ cleverhans_tutorials/mnist_tutorial_lspga.py
@@ -103,8 +103,9 @@ def mnist_tutorial(train_start=0, train_end=60000, test_start=0,
         'batch_size': batch_size,
         'learning_rate': learning_rate
     }
-    lspga_params = {'eps': 0.3, 'levels':16, 'steps':10, 'attack_step':1.0,
-                    'noisy_grads': False, thermometer: True}
+    lspga_params = {'eps': 0.3, 'levels':levels, 'steps':steps,
+                    'attack_step':1.0, 'noisy_grads': False,
+                    'thermometer': thermometer}
     rng = np.random.RandomState([2017, 8, 30])
 
     if clean_train:
