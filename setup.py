12e1edfc4a6bad548b55303f308548515b7780e0
shuolongbj <ru@aijia.io>
2018-02-04 00:54:34 +0800

    release: v2.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 
 setup(name='cleverhans',
-      version='1.0.0',
+      version='2.0.0',
       url='https://github.com/tensorflow/cleverhans',
       license='MIT',
       install_requires=[
