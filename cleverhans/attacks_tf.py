57931a44ecab1d1f3480b037a69878446430a5cf
Jonathan Uesato <juesato@mit.edu>
2018-03-02 20:18:02 +0000

    Test commit to trigger Travis build

--- cleverhans/attacks_tf.py
+++ cleverhans/attacks_tf.py
@@ -1484,4 +1484,5 @@ class LBFGS_attack(object):
 
         # return the best solution found
         o_bestl2 = np.array(o_bestl2)
+        # comment that doesn't do anything
         return o_bestattack
