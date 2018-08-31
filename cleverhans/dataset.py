b91c0905bff8c1184286eb6ba3161a88ac2a0475
Ian Goodfellow <goodfellow@google.com>
2018-08-30 17:09:58 -0700

    changes from review

--- cleverhans/dataset.py
+++ cleverhans/dataset.py
@@ -18,21 +18,21 @@ class Dataset(object):
     # The number of classes in the dataset. Should be specified by subclasses.
     NB_CLASSES = None
 
+    def __init__(self, **kwargs):
+        if "self" in kwargs:
+          del kwargs[self]
+        self.kwargs = kwargs
+
     def get_factory(self):
         """Returns a picklable callable that recreates the dataset.
         """
-
-        if hasattr(self, 'args'):
-            args = self.args
-        else:
-            args = []
 
         if hasattr(self, 'kwargs'):
             kwargs = self.kwargs
         else:
             kwargs = {}
 
-        return Factory(type(self), args, kwargs)
+        return Factory(type(self), kwargs)
 
 
 class MNIST(Dataset):
@@ -42,8 +42,8 @@ class MNIST(Dataset):
 
     def __init__(self, train_start=0, train_end=60000, test_start=0,
                  test_end=10000, center=False):
+        super(MNIST, self).__init__(**locals())
         self.kwargs = locals()
-        del self.kwargs["self"]
         x_train, y_train, x_test, y_test = data_mnist(train_start=train_start,
                                                       train_end=train_end,
                                                       test_start=test_start,
@@ -66,8 +66,7 @@ class CIFAR10(Dataset):
 
     def __init__(self, train_start=0, train_end=60000, test_start=0,
                  test_end=10000, center=False):
-        self.kwargs = locals()
-        del self.kwargs["self"]
+        super(CIFAR10, self).__init__(**locals())
         packed = data_cifar10(train_start=train_start,
                               train_end=train_end,
                               test_start=test_start,
@@ -91,13 +90,12 @@ class Factory(object):
 
     def __init__(self, cls, args, kwargs):
         self.cls = cls
-        self.args = args
         self.kwargs = kwargs
 
     def __call__(self):
         """Returns the created object.
         """
-        return self.cls(*self.args, **self.kwargs)
+        return self.cls(**self.kwargs)
 
 
 def data_cifar10(train_start=0, train_end=50000, test_start=0, test_end=10000):
@@ -118,29 +116,29 @@ def data_cifar10(train_start=0, train_end=50000, test_start=0, test_end=10000):
     nb_classes = 10
 
     # the data, shuffled and split between train and test sets
-    (X_train, y_train), (X_test, y_test) = cifar10.load_data()
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
     if keras.backend.image_dim_ordering() == 'th':
-        X_train = X_train.reshape(X_train.shape[0], 3, img_rows, img_cols)
-        X_test = X_test.reshape(X_test.shape[0], 3, img_rows, img_cols)
+        x_train = x_train.reshape(x_train.shape[0], 3, img_rows, img_cols)
+        x_test = x_test.reshape(x_test.shape[0], 3, img_rows, img_cols)
     else:
-        X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, 3)
-        X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 3)
-    X_train = X_train.astype('float32')
-    X_test = X_test.astype('float32')
-    X_train /= 255
-    X_test /= 255
-    print('X_train shape:', X_train.shape)
-    print(X_train.shape[0], 'train samples')
-    print(X_test.shape[0], 'test samples')
+        x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 3)
+        x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 3)
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
+    x_train /= 255
+    x_test /= 255
+    print('x_train shape:', x_train.shape)
+    print(x_train.shape[0], 'train samples')
+    print(x_test.shape[0], 'test samples')
 
     # convert class vectors to binary class matrices
-    Y_train = np_utils.to_categorical(y_train, nb_classes)
-    Y_test = np_utils.to_categorical(y_test, nb_classes)
+    y_train = np_utils.to_categorical(y_train, nb_classes)
+    y_test = np_utils.to_categorical(y_test, nb_classes)
 
-    X_train = X_train[train_start:train_end, :, :, :]
-    Y_train = Y_train[train_start:train_end, :]
-    X_test = X_test[test_start:test_end, :]
-    Y_test = Y_test[test_start:test_end, :]
+    x_train = x_train[train_start:train_end, :, :, :]
+    y_train = y_train[train_start:train_end, :]
+    x_test = x_test[test_start:test_end, :]
+    y_test = y_test[test_start:test_end, :]
 
-    return X_train, Y_train, X_test, Y_test
+    return x_train, y_train, x_test, y_test
