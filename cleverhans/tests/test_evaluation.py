73189857d8df2205ab44891e23fa15adee7c2ab8
Aidan Gomez <aidan.n.gomez@gmail.com>
2018-09-15 15:31:48 -0400

    use two spaces

--- cleverhans/tests/test_evaluation.py
+++ cleverhans/tests/test_evaluation.py
@@ -3,10 +3,10 @@ from cleverhans.model import Model
 
 
 def test_cache():
-    # Test that _CorrectFactory can be cached
-    model = Model()
-    factory_1 = _CorrectFactory(model)
-    factory_2 = _CorrectFactory(model)
-    cache = {}
-    cache[factory_1] = True
-    assert factory_2 in cache
+  # Test that _CorrectFactory can be cached
+  model = Model()
+  factory_1 = _CorrectFactory(model)
+  factory_2 = _CorrectFactory(model)
+  cache = {}
+  cache[factory_1] = True
+  assert factory_2 in cache
