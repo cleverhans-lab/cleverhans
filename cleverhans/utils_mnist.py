e8ecabab07c18d6530b412f93cc7f04861e60961
Fabiano <ftarlao@gmail.com>
2018-09-19 15:20:04 +0200

    PEP8 Style for previous commit

--- cleverhans/utils_mnist.py
+++ cleverhans/utils_mnist.py
@@ -61,8 +61,8 @@ def download_and_parse_mnist_file(file_name, datadir=None, force=False):
             0x0e: 'd'}
         data_type = hex_to_data_type[data_type]
 
-        #data_type unicode to ascii conversion (Python2 fix)
-        data_type = data_type.encode('ascii','ignore')
+        # data_type unicode to ascii conversion (Python2 fix)
+        data_type = data_type.encode('ascii', 'ignore')
 
         dim_sizes = struct.unpack(
             '>' + 'I' * n_dims,
