6ef9fc2069759d173c0b750631f3860fd616ee56
Ian Goodfellow <goodfellow@google.com>
2018-09-27 16:25:19 -0700

    set up pylint tests

--- cleverhans/devtools/tests/test_format.py
+++ cleverhans/devtools/tests/test_format.py
@@ -7,21 +7,40 @@ from __future__ import print_function
 from nose.plugins.skip import SkipTest
 
 import os
+import subprocess
 
 import cleverhans
 from cleverhans.devtools.tests.docscrape import docstring_errors
 from cleverhans.devtools.list_files import list_files
-from pycodestyle import StyleGuide
+from cleverhans.utils import shell_call
 
 # Enter a manual list of files that are allowed to violate PEP8 here
 whitelist_pep8 = [
+    # This file is broken but could be fixed
+    "../examples/multigpu_advtrain/test_attack_multigpu.py"
 ]
 
-# The NIPS 2017 competition code is allowed to violate PEP8 because it
-# follows the Google style guide instead (e.g., 2 spaces instead of 4)
+# We don't want to test RL-attack because it has so many dependencies
+# not used elsewhere, and pylint wants to import them all
 whitelist_pep8.extend([os.path.relpath(path, cleverhans.__path__[0])
-                       for path in list_files()
-                       if "nips17_adversarial_competition" in path])
+                       for path in list_files(".py") if "RL-attack" in path])
+# Similarly, we don't want to require robust_vision_benchmark installed
+whitelist_pep8.extend([os.path.relpath(path, cleverhans.__path__[0])
+                       for path in list_files(".py")
+                       if "robust_vision_benchmark" in path])
+# Similarly, we don't want to require that cloud be installed
+whitelist_pep8.extend([os.path.relpath(path, cleverhans.__path__[0])
+                       for path in list_files(".py")
+                       if "cloud_client" in path])
+# This example has more dependencies too
+whitelist_pep8.extend([os.path.relpath(path, cleverhans.__path__[0])
+                       for path in list_files(".py")
+                       if "facenet_adversarial_faces" in path])
+# This too
+whitelist_pep8.extend([os.path.relpath(path, cleverhans.__path__[0])
+                       for path in list_files(".py")
+                       if "madry_lab_challenges" in path])
+
 
 
 whitelist_docstrings = [
@@ -32,17 +51,31 @@ def test_format_pep8():
   """
   Test if pep8 is respected.
   """
-  pep8_checker = StyleGuide()
   files_to_check = []
+  module_dir = cleverhans.__path__[0]
   for path in list_files(".py"):
-    rel_path = os.path.relpath(path, cleverhans.__path__[0])
+    rel_path = os.path.relpath(path, module_dir)
     if rel_path in whitelist_pep8:
       continue
     else:
       files_to_check.append(path)
-  report = pep8_checker.check_files(files_to_check)
-  if report.total_errors > 0:
-    raise AssertionError("PEP8 Format not respected")
+  repo_dir = os.path.join(module_dir, os.pardir)
+  rcpath = os.path.join(repo_dir, '.pylintrc')
+  assert os.path.exists(rcpath)
+  bad_out = ""
+  for f in files_to_check:
+    # We must run pylint via the command line and subprocess because of
+    # problems with the pylint module.
+    # The documentation claims you can run it as a python module, but
+    # the documentation is wrong: https://github.com/PyCQA/pylint/issues/1870
+    # If you run the version described in the linked issue, pylint
+    # calls sys.exit once it is done, so it kills the test.
+    try:
+      out = shell_call(['pylint', '--rcfile', rcpath, f])
+    except subprocess.CalledProcessError as e:
+      bad_out = bad_out + "\n" + e.output
+  if len(bad_out) > 0:
+    raise ValueError(bad_out)
 
 
 def print_files_information_pep8():
@@ -51,8 +84,12 @@ def print_files_information_pep8():
   list of files which do not respect PEP8 formatting that aren't in the
   whitelist
   """
+
+  raise NotImplementedError("Broken by move to TensorFlow style")
+
   infracting_files = []
   non_infracting_files = []
+  # We must replace StyleGuide with pylint, as is done in the tests
   pep8_checker = StyleGuide(quiet=True)
   for path in list_files(".py"):
     number_of_infractions = pep8_checker.input_file(path)
