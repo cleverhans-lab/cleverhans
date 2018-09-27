ca954cff99765fe0658aa1247dd4bb5b6184fc7e
Ian Goodfellow <goodfellow@google.com>
2018-09-27 09:42:25 -0700

    autopep8_all.py (The script to turn the whole library tf style)

---
+++ cleverhans/devtools/autopep8_all.py
@@ -0,0 +1,17 @@
+"""
+Run this script to run autopep8 on everything in the library
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+from cleverhans.devtools.list_files import list_files
+from cleverhans.utils import shell_call
+
+for f in list_files(".py"):
+
+  command = ["autopep8", "-i", "--indent-size", "2",  f]
+  print(command)
+  shell_call(command)
