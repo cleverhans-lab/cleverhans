e67af1f93b8b84036c364815c6ba591df8f1555b
Ian Goodfellow <goodfellow@google.com>
2018-10-12 21:06:30 +0000

    pylint

--- cleverhans/evaluation.py
+++ cleverhans/evaluation.py
@@ -264,8 +264,8 @@ def batch_eval_multi_worker(sess, graph_factory, numpy_inputs, batch_size=None,
   excess = needed_m - m
   if excess > m:
     raise NotImplementedError(("Your batch size (%(batch_size)d) is bigger"
-                              " than the dataset (%(m)d), this function is "
-                              "probably overkill.") % locals())
+                               " than the dataset (%(m)d), this function is "
+                               "probably overkill.") % locals())
 
   def pad(array):
     """Pads an array with replicated examples to have `excess` more entries"""
