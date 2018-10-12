035118a8a39e15177addc4d031f38fddf88ac3c7
Ian Goodfellow <goodfellow@google.com>
2018-10-12 20:44:09 +0000

    add cleverhans.utils_tf.silence

--- cleverhans/utils_tf.py
+++ cleverhans/utils_tf.py
@@ -520,3 +520,10 @@ def get_available_gpus():
   """
   local_device_protos = device_lib.list_local_devices()
   return [x.name for x in local_device_protos if x.device_type == 'GPU']
+
+
+def silence():
+  """
+  Silences tensorflaw's default printed messages
+  """
+  os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
