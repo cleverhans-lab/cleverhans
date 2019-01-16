3e7d024796429f2ed5a970c5b197d9ced78ce1ee
Aditi Raghunathan <aditir@rn31-cl-07-imi4.stanford.edu>
2019-01-15 17:48:36 -0800

    Changes to certification and neural net params

--- cleverhans/experimental/certification/certify.py
+++ cleverhans/experimental/certification/certify.py
@@ -20,7 +20,11 @@ flags.DEFINE_string('checkpoint', None,
                     'Path of checkpoint with trained model to verify')
 flags.DEFINE_string('model_json', None,
                     'Path of json file with model description')
-flags.DEFINE_string('init_dual_file', None,
+flags.DEFINE_string('model_logits', None, 
+                    'Path of clean logits')
+flags.DEFINE_integer('input_index', None, 
+                    'Index of test input (to compute logits)')
+flags.DEFINE_string('init_dual_folder', None,
                     'Path of numpy file with dual variables to initialize')
 flags.DEFINE_string('test_input', None,
                     'Path of numpy file with test input to certify')
@@ -74,22 +78,40 @@ flags.DEFINE_enum('verbosity', 'INFO',
                   'Logging verbosity level.')
 
 
+dataset = 'MNIST'
+
 def main(_):
   tf.logging.set_verbosity(FLAGS.verbosity)
 
+  # Reading test input and reshaping
+  with tf.gfile.Open(FLAGS.test_input) as f:
+    test_input = np.load(f)
+  test_input = np.reshape(test_input, [-1, 1])
+
+  if(dataset=='MNIST'):
+    num_rows = 28
+    num_columns = 28
+    num_channels = 1
+
   net_weights, net_biases, net_layer_types = read_weights.read_weights(
       FLAGS.checkpoint, FLAGS.model_json)
+  input_shape = [num_rows, num_columns, num_channels]
   nn_params = neural_net_params.NeuralNetParams(
-      net_weights, net_biases, net_layer_types)
+      net_weights, net_biases, net_layer_types, input_shape)
+
+  # Creating feedforward object for debugging 
+  net_weights, net_biases, net_layer_types = read_weights.read_weights(
+    FLAGS.checkpoint, FLAGS.model_json, [num_rows, num_columns, num_channels], 
+    CONV2FF=True)
+  nn_params_ff = neural_net_params.NeuralNetParams(
+    net_weights, net_biases, net_layer_types)
+
+
   tf.logging.info('Loaded neural network with size of layers: %s',
                   nn_params.sizes)
   dual_var = utils.initialize_dual(nn_params, FLAGS.init_dual_file,
                                    init_nu=FLAGS.init_nu)
-  # Reading test input and reshaping
-  with tf.gfile.Open(FLAGS.test_input) as f:
-    test_input = np.load(f)
-  test_input = np.reshape(test_input, [np.size(test_input), 1])
-
+  
   if FLAGS.adv_class == -1:
     start_class = 0
     end_class = FLAGS.num_classes
@@ -100,7 +122,41 @@ def main(_):
     tf.logging.info('Running certification for adversarial class %d', adv_class)
     if adv_class == FLAGS.true_class:
       continue
-    dual = dual_formulation.DualFormulation(dual_var,
+
+    optimization_params = {
+    'init_penalty': FLAGS.init_penalty,
+    'large_eig_num_steps': FLAGS.large_eig_num_steps,
+    'small_eig_num_steps': FLAGS.small_eig_num_steps,
+    'inner_num_steps': FLAGS.inner_num_steps,
+    'outer_num_steps': FLAGS.outer_num_steps,
+    'beta': FLAGS.beta,
+    'smoothness_parameter': FLAGS.smoothness_parameter,
+    'eig_learning_rate': FLAGS.eig_learning_rate,
+    'optimizer': FLAGS.optimizer,
+    'init_learning_rate': FLAGS.init_learning_rate,
+    'learning_rate_decay': FLAGS.learning_rate_decay,
+    'momentum_parameter': FLAGS.momentum_parameter,
+    'print_stats_steps': FLAGS.print_stats_steps,
+    'stats_folder': FLAGS.stats_folder,
+    'projection_steps': FLAGS.projection_steps}
+
+    config = tf.ConfigProto(
+      device_count={'GPU':0})
+  
+    with tf.Session() as sess:
+      sess.run(tf.global_variables_initializer())
+      # Checking that all the weights are loaded correctly 
+      nn_test_output = nn_params.nn_output(test_input, FLAGS.true_class, adv_class)
+      current_test_output = sess.run(nn_test_output)
+      test_logits = np.load(FLAGS.model_logits)
+      true_test_output = (test_logits[FLAGS.input_index, adv_class] - 
+        test_logits[FLAGS.input_index, FLAGS.true_class])
+
+      if(np.abs(true_test_output - current_test_output) > 1E-3):
+        print('Forward passes do not match with difference ', np.abs(true_test_output - current_test_output))
+        exit()
+
+      dual = dual_formulation.DualFormulation(sess,
                                             nn_params,
                                             test_input,
                                             FLAGS.true_class,
@@ -108,29 +164,13 @@ def main(_):
                                             FLAGS.input_minval,
                                             FLAGS.input_maxval,
                                             FLAGS.epsilon)
-    dual.set_differentiable_objective()
-    dual.get_full_psd_matrix()
-    optimization_params = {
-        'init_penalty': FLAGS.init_penalty,
-        'large_eig_num_steps': FLAGS.large_eig_num_steps,
-        'small_eig_num_steps': FLAGS.small_eig_num_steps,
-        'inner_num_steps': FLAGS.inner_num_steps,
-        'outer_num_steps': FLAGS.outer_num_steps,
-        'beta': FLAGS.beta,
-        'smoothness_parameter': FLAGS.smoothness_parameter,
-        'eig_learning_rate': FLAGS.eig_learning_rate,
-        'optimizer': FLAGS.optimizer,
-        'init_learning_rate': FLAGS.init_learning_rate,
-        'learning_rate_decay': FLAGS.learning_rate_decay,
-        'momentum_parameter': FLAGS.momentum_parameter,
-        'print_stats_steps': FLAGS.print_stats_steps,
-        'stats_folder': FLAGS.stats_folder,
-        'projection_steps': FLAGS.projection_steps}
-    with tf.Session() as sess:
-      sess.run(tf.global_variables_initializer())
+      dual.initialize_dual(str(adv_class), FLAGS.init_dual_folder, 
+          init_nu=FLAGS.init_nu)
+
       optimization_object = optimization.Optimization(dual,
                                                       sess,
-                                                      optimization_params)
+                                                      optimization_params. 
+                                                      nn_params_ff)
       optimization_object.prepare_one_step()
       is_cert_found = optimization_object.run_optimization()
       if not is_cert_found:
@@ -138,5 +178,6 @@ def main(_):
         exit()
   print('Example successfully verified')
 
+    
 if __name__ == '__main__':
   tf.app.run(main)
--- cleverhans/experimental/certification/neural_net_params.py
+++ cleverhans/experimental/certification/neural_net_params.py
@@ -6,12 +6,14 @@ from __future__ import print_function
 import numpy as np
 import tensorflow as tf
 
+STRIDE = 2
+PADDING = 'SAME'
 
 class NeuralNetParams(object):
   """NeuralNetParams is a class that interfaces the verification code with
   the neural net parameters (weights)"""
 
-  def __init__(self, net_weights, net_biases, net_layer_types):
+  def __init__(self, net_weights, net_biases, net_layer_types, input_shape=None):
     """Function to initialize NeuralNetParams class.
 
     Args:
@@ -24,6 +26,7 @@ class NeuralNetParams(object):
          'ff_relu': Simple feedforward layer with ReLU activations
          'ff_conv': Convolution layer with no activation
          'ff_conv_relu': Convolution layer with ReLU activation
+       input_shape = [num_rows, num_columns, num_channels] at the input layer
 
     Raises:
       ValueError: the input lists of net params are not of the same length
@@ -38,18 +41,54 @@ class NeuralNetParams(object):
     self.biases = []
     self.layer_types = []
     self.sizes = []
+    self.input_shapes = []
+    self.output_shapes = []
+    if(input_shape is not None):
+      current_num_rows = input_shape[0]
+      current_num_columns = input_shape[1]
+      current_num_channels = input_shape[2]
+
     # Setting the sizes of the layers of the network
     # sizes[i] contains the size of x_i
     for i in range(self.num_hidden_layers):
       shape = np.shape(net_weights[i])
-      self.sizes.append(int(shape[1]))
       self.weights.append(
           tf.convert_to_tensor(net_weights[i], dtype=tf.float32))
-      self.biases.append(tf.convert_to_tensor(net_biases[i], dtype=tf.float32))
       self.layer_types.append(net_layer_types[i])
 
-    # Last layer shape
-    self.sizes.append(int(np.shape(net_weights[self.num_hidden_layers-1])[0]))
+      if(self.layer_types[i] in {'ff', 'ff_relu'}):
+        self.sizes.append(int(shape[1]))
+        # For feedforward networks, no unraveling the bias terms                                                                                 
+        small_bias = tf.convert_to_tensor(net_biases[i], dtype=tf.float32)
+        self.biases.append(tf.reshape(small_bias, [-1, 1]))
+        # Assumes that x^{i+1} = W_i x^i
+        self.input_shapes.append([int(shape[1]), 1])
+        self.output_shapes.append([int(shape[0]), 1])
+
+      # Convolution type
+      else:
+        num_filters = shape[3]
+        self.input_shapes.append([1, 
+          current_num_rows, current_num_cols, current_num_channels])
+        self.sizes.append(current_num_rows*current_num_cols*current_num_channels)
+        current_num_channels = num_filters
+        # For propagating across multiple conv layers
+        if(PADDING == 'SAME'):
+          current_num_rows = int(current_num_rows/STRIDE)
+          current_num_cols = int(current_num_cols/STRIDE)
+        self.output_shapes.append(
+          [1, current_num_rows, current_num_cols, current_num_channels])
+
+        # For conv networks, unraveling the bias terms                                                                                           
+        small_bias = tf.convert_to_tensor(net_biases[i], dtype=tf.float32)
+        large_bias = tf.tile(tf.reshape(small_bias, [-1, 1]), 
+          [current_num_rows*current_num_cols, 1])
+        self.biases.append(large_bias)
+
+    # Last layer shape: always ff 
+    final_dim = int(np.shape(net_weights[self.num_hidden_layers])[1])
+    self.sizes.append(final_dim)
+    self.input_shapes.append([final_dim, 1])
     self.final_weights = tf.convert_to_tensor(
         net_weights[self.num_hidden_layers], dtype=tf.float32)
     self.final_bias = tf.convert_to_tensor(
@@ -73,14 +112,67 @@ class NeuralNetParams(object):
     if(layer_index < 0 or layer_index > self.num_hidden_layers):
       raise ValueError('Invalid layer index')
 
-    if(self.layer_types[layer_index] == 'ff' or
-       self.layer_types[layer_index] == 'ff_relu'):
-      if is_transpose:
-        return tf.matmul(tf.transpose(self.weights[layer_index]), vector)
-      elif is_abs:
-        return tf.matmul(tf.abs(self.weights[layer_index]), vector)
+
+    layer_type = self.layer_types[layer_index]
+    # Reshaping the input for convolution appropriately
+    if is_transpose:
+      vector = tf.reshape(vector, self.output_shapes[layer_index])
+      if (layer_type in {'ff', 'ff_relu'}):
+        return_vector = tf.matmul(tf.transpose(self.weights[layer_index]), vector)
+      elif(layer_type in {'conv', 'conv_relu'}):
+        return_vector = tf.nn.conv2d_transpose(vector, self.weights[layer_index],
+                                               output_shape=self.input_shapes[layer_index],
+                                               strides=[1, STRIDE, STRIDE, 1],
+                                               padding=PADDING)
       else:
-        return tf.matmul(self.weights[layer_index], vector)
+        raise NotImplementedError('Unsupported layer type: {0}'.format(self.layer_type))
+      return tf.reshape(return_vector, (self.sizes[layer_index + 1], 1))
+
+    elif is_abs:
+      vector = tf.reshape(vector, self.input_shapes[layer_index])
+      if(layer_type in {'ff', 'ff_relu'}):
+        return_vector = tf.matmul(tf.abs(self.weights[layer_index], vector))
+      elif(layer_type in {'conv', 'conv_relu'}):
+        return_vector = tf.nn.conv2d(vector, 
+                                    tf.abs(self.weights[layer_index]), 
+                                    strides=[1, STRIDE, STRIDE, 1],
+                                    padding=PADDING)
+      else:
+        raise NotImplementedError('Unsupported layer type: {0}'.format(self.layer_type))
+      return tf.reshape(return_vector, (self.sizes[layer_index + 1], 1))
+
+    # Simple forward pass 
+    else:
+      vector = tf.reshape(vector, self.input_shapes[layer_index])
+      if(layer_type in {'ff', 'ff_relu'}):
+        return_vector = tf.matmul(self.weights[layer_index], vector)
+      elif (layer_type in {'conv', 'conv_relu'}):
+        return_vector = tf.nn.conv2d(vector, self.weights[layer_index], 
+                                    strides=[1, STRIDE, STRIDE, 1],
+                                    padding=PADDING)
+      else:
+        raise NotImplementedError('Unsupported layer type: {0}'.format(self.layer_type))
+      return tf.reshape(return_vector, (self.sizes[layer_index + 1], 1))
+
 
     raise NotImplementedError('Unsupported layer type: {0}'.format(
         self.layer_types[layer_index]))
+
+  def nn_output(self, test_input, true_class, adv_class):
+    """ Function to print the output of forward pass according the neural net class
+      Args:
+        test_input: Input to pass through the network
+        true_class: True class of the input 
+        adv_class: Adversarial class to be considered
+    """
+    activation = test_input
+    # Assumes that all the layers are relu: has to be modified for other architectures
+    for i in range(self.num_hidden_layers):
+      activation = tf.nn.relu(self.forward_pass(activation, i) + self.biases[i])
+    # Final layer                                                                                                                                
+    activation = tf.reshape(activation, [-1, 1])
+    final_weight = self.final_weights[adv_class, :] - self.final_weights[true_class, :]
+    final_weight = tf.reshape(final_weight, [-1, 1])
+    final_constant = (self.final_bias[adv_class] - self.final_bias[true_class])
+    output = tf.reduce_sum(tf.multiply(activation, final_weight)) + final_constant
+    return output
