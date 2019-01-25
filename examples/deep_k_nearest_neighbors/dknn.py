a7f01320c1e5d7d7cd8bbeab5ec904f0381d308d
npapernot <papernot@google.com>
2019-01-25 00:49:24 +0000

    autopep8

--- examples/deep_k_nearest_neighbors/dknn.py
+++ examples/deep_k_nearest_neighbors/dknn.py
@@ -11,7 +11,7 @@ from __future__ import unicode_literals
 import os
 import matplotlib
 if 'DISPLAY' not in os.environ:
-    matplotlib.use('Agg')
+  matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 from bisect import bisect_left
@@ -86,13 +86,16 @@ class DkNNModel(Model):
     """
     Initializes locality-sensitive hashing with FALCONN to find nearest neighbors in training data.
     """
-    self.query_objects = {}  # contains the object that can be queried to find nearest neighbors at each layer.
-    self.centers = {}  # mean of training data representation per layer (that needs to be substracted before LSH).
+    self.query_objects = {
+    }  # contains the object that can be queried to find nearest neighbors at each layer.
+    # mean of training data representation per layer (that needs to be substracted before LSH).
+    self.centers = {}
     for layer in self.layers:
       assert self.nb_tables >= self.neighbors
 
       # Normalize all the lenghts, since we care about the cosine similarity.
-      self.train_activations_lsh[layer] /= np.linalg.norm(self.train_activations_lsh[layer], axis=1).reshape(-1, 1)
+      self.train_activations_lsh[layer] /= np.linalg.norm(
+          self.train_activations_lsh[layer], axis=1).reshape(-1, 1)
 
       # Center the dataset and the queries: this improves the performance of LSH quite a bit.
       center = np.mean(self.train_activations_lsh[layer], axis=0)
@@ -107,7 +110,8 @@ class DkNNModel(Model):
       params_cp.l = self.nb_tables
       params_cp.num_rotations = 2  # for dense set it to 1; for sparse data set it to 2
       params_cp.seed = 5721840
-      params_cp.num_setup_threads = 0  # we want to use all the available threads to set up
+      # we want to use all the available threads to set up
+      params_cp.num_setup_threads = 0
       params_cp.storage_hash_table = falconn.StorageHashTable.BitPackedFlatHashTable
 
       # we build 18-bit hashes so that each table has
@@ -136,14 +140,17 @@ class DkNNModel(Model):
       # Pre-process representations of data to normalize and remove training data mean.
       data_activations_layer = copy.copy(data_activations[layer])
       nb_data = data_activations_layer.shape[0]
-      data_activations_layer /= np.linalg.norm(data_activations_layer, axis=1).reshape(-1, 1)
+      data_activations_layer /= np.linalg.norm(
+          data_activations_layer, axis=1).reshape(-1, 1)
       data_activations_layer -= self.centers[layer]
 
       # Use FALCONN to find indices of nearest neighbors in training data.
-      knns_ind[layer] = np.zeros((data_activations_layer.shape[0], self.neighbors), dtype=np.int32)
+      knns_ind[layer] = np.zeros(
+          (data_activations_layer.shape[0], self.neighbors), dtype=np.int32)
       knn_errors = 0
       for i in range(data_activations_layer.shape[0]):
-        query_res = self.query_objects[layer].find_k_nearest_neighbors(data_activations_layer[i], self.neighbors)
+        query_res = self.query_objects[layer].find_k_nearest_neighbors(
+            data_activations_layer[i], self.neighbors)
         try:
           knns_ind[layer][i, :] = query_res
         except:
@@ -153,7 +160,8 @@ class DkNNModel(Model):
       # Find labels of neighbors found in the training data.
       knns_labels[layer] = np.zeros((nb_data, self.neighbors), dtype=np.int32)
       for data_id in range(nb_data):
-        knns_labels[layer][data_id, :] = self.train_labels[knns_ind[layer][data_id]]
+        knns_labels[layer][data_id,
+                           :] = self.train_labels[knns_ind[layer][data_id]]
 
     return knns_ind, knns_labels
 
@@ -167,13 +175,16 @@ class DkNNModel(Model):
     knns_not_in_class = np.zeros((nb_data, self.nb_classes), dtype=np.int32)
     for i in range(nb_data):
       # Compute number of nearest neighbors per class
-      knns_in_class = np.zeros((len(self.layers), self.nb_classes), dtype=np.int32)
+      knns_in_class = np.zeros(
+          (len(self.layers), self.nb_classes), dtype=np.int32)
       for layer_id, layer in enumerate(self.layers):
-        knns_in_class[layer_id, :] = np.bincount(knns_labels[layer][i], minlength=self.nb_classes)
+        knns_in_class[layer_id, :] = np.bincount(
+            knns_labels[layer][i], minlength=self.nb_classes)
 
       # Compute number of knns in other class than class_id
       for class_id in range(self.nb_classes):
-        knns_not_in_class[i, class_id] = np.sum(knns_in_class) - np.sum(knns_in_class[:, class_id])
+        knns_not_in_class[i, class_id] = np.sum(
+            knns_in_class) - np.sum(knns_in_class[:, class_id])
     return knns_not_in_class
 
   def preds_conf_cred(self, knns_not_in_class):
@@ -192,7 +203,8 @@ class DkNNModel(Model):
 
       for class_id in range(self.nb_classes):
         # p-value of (test point, candidate label)
-        p_value[class_id] = (float(self.nb_cali) - bisect_left(self.cali_nonconformity, knns_not_in_class[i, class_id])) / float(self.nb_cali)
+        p_value[class_id] = (float(self.nb_cali) - bisect_left(
+            self.cali_nonconformity, knns_not_in_class[i, class_id])) / float(self.nb_cali)
 
       preds_knn[i] = np.argmax(p_value)
       confs[i, preds_knn[i]] = 1. - p_value[np.argsort(p_value)[-2]]
@@ -204,7 +216,8 @@ class DkNNModel(Model):
     Performs a forward pass through the DkNN on an numpy array of data.
     """
     if not self.calibrated:
-      raise Error("DkNN needs to be calibrated by calling DkNNModel.calibrate method once before inferring.")
+      raise Error(
+          "DkNN needs to be calibrated by calling DkNNModel.calibrate method once before inferring.")
     data_activations = self.get_activations(data_np)
     _, knns_labels = self.find_train_knns(data_activations)
     knns_not_in_class = self.nonconformity(knns_labels)
@@ -230,9 +243,12 @@ class DkNNModel(Model):
     self.cali_labels = cali_labels
 
     print("Starting calibration of DkNN.")
-    cali_knns_ind, cali_knns_labels = self.find_train_knns(self.cali_activations)
-    assert all([v.shape == (self.nb_cali, self.neighbors) for v in cali_knns_ind.itervalues()])
-    assert all([v.shape == (self.nb_cali, self.neighbors) for v in cali_knns_labels.itervalues()])
+    cali_knns_ind, cali_knns_labels = self.find_train_knns(
+        self.cali_activations)
+    assert all([v.shape == (self.nb_cali, self.neighbors)
+                for v in cali_knns_ind.itervalues()])
+    assert all([v.shape == (self.nb_cali, self.neighbors)
+                for v in cali_knns_labels.itervalues()])
 
     cali_knns_not_in_class = self.nonconformity(cali_knns_labels)
     cali_knns_not_in_l = np.zeros(self.nb_cali, dtype=np.int32)
@@ -250,73 +266,73 @@ class DkNNModel(Model):
 
 
 def plot_reliability_diagram(confidence, labels, filepath):
-    """
-    Takes in confidence values for predictions and correct
-    labels for the data, plots a reliability diagram.
-    :param confidence: nb_samples x nb_classes (e.g., output of softmax)
-    :param labels: vector of nb_samples
-    :param filepath: where to save the diagram
-    :return:
-    """
-    assert len(confidence.shape) == 2
-    assert len(labels.shape) == 1
-    assert confidence.shape[0] == labels.shape[0]
-    print('Saving reliability diagram at: ' + str(filepath))
-    if confidence.max() <= 1.:
+  """
+  Takes in confidence values for predictions and correct
+  labels for the data, plots a reliability diagram.
+  :param confidence: nb_samples x nb_classes (e.g., output of softmax)
+  :param labels: vector of nb_samples
+  :param filepath: where to save the diagram
+  :return:
+  """
+  assert len(confidence.shape) == 2
+  assert len(labels.shape) == 1
+  assert confidence.shape[0] == labels.shape[0]
+  print('Saving reliability diagram at: ' + str(filepath))
+  if confidence.max() <= 1.:
         # confidence array is output of softmax
-        bins_start = [b / 10. for b in xrange(0, 10)]
-        bins_end = [b / 10. for b in xrange(1, 11)]
-        bins_center = [(b + .5) / 10. for b in xrange(0, 10)]
-        preds_conf = np.max(confidence, axis=1)
-        preds_l = np.argmax(confidence, axis=1)
+    bins_start = [b / 10. for b in xrange(0, 10)]
+    bins_end = [b / 10. for b in xrange(1, 11)]
+    bins_center = [(b + .5) / 10. for b in xrange(0, 10)]
+    preds_conf = np.max(confidence, axis=1)
+    preds_l = np.argmax(confidence, axis=1)
+  else:
+    raise ValueError('Confidence values go above 1.')
+
+  print(preds_conf.shape, preds_l.shape)
+
+  # Create var for reliability diagram
+  # Will contain mean accuracies for each bin
+  reliability_diag = []
+  num_points = []  # keeps the number of points in each bar
+
+  # Find average accuracy per confidence bin
+  for bin_start, bin_end in zip(bins_start, bins_end):
+    above = preds_conf >= bin_start
+    if bin_end == 1.:
+      below = preds_conf <= bin_end
     else:
-        raise ValueError('Confidence values go above 1.')
+      below = preds_conf < bin_end
+    mask = np.multiply(above, below)
+    num_points.append(np.sum(mask))
+    bin_mean_acc = max(0, np.mean(preds_l[mask] == labels[mask]))
+    reliability_diag.append(bin_mean_acc)
 
-    print(preds_conf.shape, preds_l.shape)
+  # Plot diagram
+  assert len(reliability_diag) == len(bins_center)
+  print(reliability_diag)
+  print(bins_center)
+  print(num_points)
+  fig, ax1 = plt.subplots()
+  barlist = ax1.bar(bins_center, reliability_diag, width=.1, alpha=0.8)
+  plt.xlim([0, 1.])
+  ax1.set_ylim([0, 1.])
 
-    # Create var for reliability diagram
-    # Will contain mean accuracies for each bin
-    reliability_diag = []
-    num_points = []  # keeps the number of points in each bar
+  ax2 = ax1.twinx()
+  print(sum(num_points))
+  ax2.plot(bins_center, num_points, color='r', linestyle='-', linewidth=7.0)
+  ax2.set_ylabel('Number of points in the data', fontsize=16, color='r')
 
-    # Find average accuracy per confidence bin
-    for bin_start, bin_end in zip(bins_start, bins_end):
-        above = preds_conf >= bin_start
-        if bin_end == 1.:
-            below = preds_conf <= bin_end
-        else:
-            below = preds_conf < bin_end
-        mask = np.multiply(above, below)
-        num_points.append(np.sum(mask))
-        bin_mean_acc = max(0, np.mean(preds_l[mask] == labels[mask]))
-        reliability_diag.append(bin_mean_acc)
-
-    # Plot diagram
-    assert len(reliability_diag) == len(bins_center)
-    print(reliability_diag)
-    print(bins_center)
-    print(num_points)
-    fig, ax1 = plt.subplots()
-    barlist = ax1.bar(bins_center, reliability_diag, width=.1, alpha=0.8)
-    plt.xlim([0, 1.])
-    ax1.set_ylim([0, 1.])
-
-    ax2 = ax1.twinx()
-    print(sum(num_points))
-    ax2.plot(bins_center, num_points, color='r', linestyle='-', linewidth=7.0)
-    ax2.set_ylabel('Number of points in the data', fontsize=16, color='r')
-
-    if len(np.argwhere(confidence[0] != 0.)) == 1:
-        # This is a DkNN diagram
-        ax1.set_xlabel('Prediction Credibility', fontsize=16)
-    else:
-        # This is a softmax diagram
-        ax1.set_xlabel('Prediction Confidence', fontsize=16)
-    ax1.set_ylabel('Prediction Accuracy', fontsize=16)
-    ax1.tick_params(axis='both', labelsize=14)
-    ax2.tick_params(axis='both', labelsize=14, colors='r')
-    fig.tight_layout()
-    plt.savefig(filepath, bbox_inches='tight')
+  if len(np.argwhere(confidence[0] != 0.)) == 1:
+    # This is a DkNN diagram
+    ax1.set_xlabel('Prediction Credibility', fontsize=16)
+  else:
+    # This is a softmax diagram
+    ax1.set_xlabel('Prediction Confidence', fontsize=16)
+  ax1.set_ylabel('Prediction Accuracy', fontsize=16)
+  ax1.tick_params(axis='both', labelsize=14)
+  ax2.tick_params(axis='both', labelsize=14, colors='r')
+  fig.tight_layout()
+  plt.savefig(filepath, bbox_inches='tight')
 
 
 def dknn_tutorial():
@@ -332,30 +348,36 @@ def dknn_tutorial():
   with tf.Session() as sess:
     with tf.variable_scope('dknn'):
       # Define input TF placeholder.
-      x = tf.placeholder(tf.float32, shape=(None, img_rows, img_cols, nchannels))
+      x = tf.placeholder(tf.float32, shape=(
+          None, img_rows, img_cols, nchannels))
       y = tf.placeholder(tf.float32, shape=(None, nb_classes))
 
       # Define a model.
       nb_filters = 64
-      model = make_basic_picklable_cnn(nb_filters=64, nb_classes=10, input_shape=(None, 28, 28, 1))
+      model = make_basic_picklable_cnn(
+          nb_filters=64, nb_classes=10, input_shape=(None, 28, 28, 1))
       preds = model.get_logits(x)
       loss = CrossEntropy(model, smoothing=0.)
 
       # Define the test set accuracy evaluation.
       def evaluate():
-        acc = model_eval(sess, x, y, preds, x_test, y_test, args={'batch_size': FLAGS.batch_size})
+        acc = model_eval(sess, x, y, preds, x_test, y_test,
+                         args={'batch_size': FLAGS.batch_size})
         print('Test accuracy on test examples: %0.4f' % acc)
 
       # Train the model
-      train_params = {'nb_epochs': FLAGS.nb_epochs, 'batch_size': FLAGS.batch_size, 'learning_rate': FLAGS.lr}
-      train(sess, loss, x_train, y_train, evaluate=evaluate, args=train_params, var_list=model.get_params())
+      train_params = {'nb_epochs': FLAGS.nb_epochs,
+                      'batch_size': FLAGS.batch_size, 'learning_rate': FLAGS.lr}
+      train(sess, loss, x_train, y_train, evaluate=evaluate,
+            args=train_params, var_list=model.get_params())
 
       # Define callable that returns a dictionary of all activations for a dataset
       def get_activations(data):
         data_activations = {}
         for layer in layers:
           layer_sym = tf.layers.flatten(model.get_layer(x, layer))
-          data_activations[layer] = batch_eval(sess, [x], [layer_sym], [data], args={'batch_size': FLAGS.batch_size})[0]
+          data_activations[layer] = batch_eval(sess, [x], [layer_sym], [data], args={
+                                               'batch_size': FLAGS.batch_size})[0]
         return data_activations
 
       # Use a holdout of the test set to simulate calibration data for the DkNN.
@@ -373,20 +395,23 @@ def dknn_tutorial():
       cali_activations = get_activations(cali_data)
 
       # Wrap the model into a DkNNModel
-      dknn = DkNNModel(FLAGS.neighbors, layers, get_activations, train_data, train_labels, nb_classes, scope='dknn')
+      dknn = DkNNModel(FLAGS.neighbors, layers, get_activations,
+                       train_data, train_labels, nb_classes, scope='dknn')
       dknn.calibrate(cali_data, cali_labels)
 
       # Generate adversarial examples
       fgsm = FastGradientMethod(model, sess=sess)
       attack_params = {'eps': .25, 'clip_min': 0., 'clip_max': 1.}
-      adv = sess.run(fgsm.generate(x, **attack_params), feed_dict={x: test_data})
+      adv = sess.run(fgsm.generate(x, **attack_params),
+                     feed_dict={x: test_data})
 
       # Test the DkNN on clean test data and FGSM test data
       for data_in, fname in zip([test_data, adv], ['test', 'adv']):
         dknn_preds = dknn.fprop_np(data_in)
         print(dknn_preds.shape)
         print(np.mean(np.argmax(dknn_preds, axis=1) == np.argmax(y_test, axis=1)))
-        plot_reliability_diagram(dknn_preds, np.argmax(y_test, axis=1), '/tmp/dknn_' + fname + '.pdf')
+        plot_reliability_diagram(dknn_preds, np.argmax(
+            y_test, axis=1), '/tmp/dknn_' + fname + '.pdf')
 
   return True
 
@@ -400,7 +425,9 @@ if __name__ == '__main__':
   flags.DEFINE_integer('batch_size', 500, 'Size of training batches')
   flags.DEFINE_float('lr', 0.001, 'Learning rate for training')
 
-  flags.DEFINE_integer('nb_cali', 750, 'Number of calibration points for the DkNN')
-  flags.DEFINE_integer('neighbors', 75, 'Number of neighbors per layer for the DkNN')
+  flags.DEFINE_integer(
+      'nb_cali', 750, 'Number of calibration points for the DkNN')
+  flags.DEFINE_integer(
+      'neighbors', 75, 'Number of neighbors per layer for the DkNN')
 
   tf.app.run()
