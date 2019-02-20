df7e3efa276782fcb05ab854214415d44e61138b
Ian Goodfellow <goodfellow@google.com>
2019-02-20 14:00:47 -0800

    some new no-docstring problems introduced to main branch by recent PRs

--- cleverhans/model_zoo/soft_nearest_neighbor_loss/SNNL_regularized_model.py
+++ cleverhans/model_zoo/soft_nearest_neighbor_loss/SNNL_regularized_model.py
@@ -1,6 +1,7 @@
 """
 A Simple Neural Net to optimize with SNNL and Cross Entropy
 """
+# pylint: disable=docstring-missing
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
