f5911125e89fa273642e044fef52498787396e4f
Shreya <ss.shankar505@gmail.com>
2019-02-26 18:02:13 -0800

    fixing

--- cleverhans/experimental/certification/tests/utils_test.py
+++ cleverhans/experimental/certification/tests/utils_test.py
@@ -5,6 +5,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import os
 import time
 import numpy as np
 import tensorflow as tf
@@ -36,35 +37,35 @@ class UtilsTest(tf.test.TestCase):
         v = -v
     np.testing.assert_almost_equal(v, [[0.9239], [-0.3827]], decimal=4)
 
-  # def basic_lanczos_test(self):
-  #   # Define vector-product functions
-  #   def _vector_prod_fn(x):
-  #     return tf.matmul(matrix, x)
-  #   def _np_vector_prod_fn(x):
-  #     return np.matmul(matrix, x)
+  def basic_lanczos_test(self):
+    # Define vector-product functions
+    def _vector_prod_fn(x):
+      return tf.matmul(matrix, x)
+    def _np_vector_prod_fn(x):
+      return np.matmul(matrix, x)
 
-  #   # Create test diagonal matrix
-  #   diagonal_entries = np.random.randint(low=0, size=TEST_DIM)
-  #   matrix = np.diag(diagonal_entries)
+    # Create test diagonal matrix
+    diagonal_entries = np.random.randint(low=0, size=TEST_DIM)
+    matrix = np.diag(diagonal_entries)
 
-  #   # Create lanczos graph nodes
-  #   min_eigen_fn = autograph.to_graph(utils.lanczos_decomp)
+    # Create lanczos graph nodes
+    min_eigen_fn = autograph.to_graph(utils.lanczos_decomp)
 
-  #   # Compare against scipy
-  #   linear_operator = LinearOperator((TEST_DIM, TEST_DIM), matvec=_np_vector_prod_fn)
-  #   min_eig_scipy, _ = eigs(linear_operator, k=1, which='SR', tol=TOL)
-  #   print("Min eig scipy: " + str(min_eig_scipy))
+    # Compare against scipy
+    linear_operator = LinearOperator((TEST_DIM, TEST_DIM), matvec=_np_vector_prod_fn)
+    min_eig_scipy, _ = eigs(linear_operator, k=1, which='SR', tol=TOL)
+    print("Min eig scipy: " + str(min_eig_scipy))
 
-  #   # Use lanczos method
-  #   with tf.Session() as sess:
-  #     alpha_hat, beta_hat, Q_hat = min_eigen_fn(_vector_prod_fn, 0, TEST_DIM, TEST_DIM/5)
-  #     # Finalize graph to make sure no new nodes are added
-  #     tf.get_default_graph().finalize()
-  #     curr_alpha_hat, curr_beta_hat, _ = sess.run([alpha_hat, beta_hat, Q_hat])
-  #     min_eig_lzs, _, _, _ = utils.eigen_tridiagonal(curr_alpha_hat, curr_beta_hat, maximum=False)
-  #     print(min_eig_lzs)
-  #     np.testing.assert_almost_equal(min_eig_lzs, min_eig_scipy, decimal=2)
-  #   tf.reset_default_graph()
+    # Use lanczos method
+    with tf.Session() as sess:
+      alpha_hat, beta_hat, Q_hat = min_eigen_fn(_vector_prod_fn, 0, TEST_DIM, TEST_DIM/5)
+      # Finalize graph to make sure no new nodes are added
+      tf.get_default_graph().finalize()
+      curr_alpha_hat, curr_beta_hat, _ = sess.run([alpha_hat, beta_hat, Q_hat])
+      min_eig_lzs, _, _, _ = utils.eigen_tridiagonal(curr_alpha_hat, curr_beta_hat, maximum=False)
+      print(min_eig_lzs)
+      np.testing.assert_almost_equal(min_eig_lzs, min_eig_scipy, decimal=2)
+    tf.reset_default_graph()
 
   def advanced_lanczos_test(self):
     print(FLAGS)
@@ -77,7 +78,7 @@ class UtilsTest(tf.test.TestCase):
       return np.matmul(matrix, x)
 
     for filename in filenames:
-      filename = './matrices/' + filename
+      filename = os.path.join(FLAGS.test_srcdir, './matrices/' + filename
       matrix = np.load(filename).astype(np.float32)
       n = matrix.shape[0]
 
