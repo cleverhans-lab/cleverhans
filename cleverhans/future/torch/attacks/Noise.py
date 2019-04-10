324b18d392a6d67e330884d528c64e2b556f86dd
Arunava <learningdroidarunava@gmail.com>
2019-04-10 20:17:50 +0530

    Fixes

--- cleverhans/future/torch/attacks/Noise.py
+++ cleverhans/future/torch/attacks/Noise.py
@@ -6,19 +6,20 @@ import numpy as np
 import torch
 from cleverhans.future.torch.attack.Attack import Attack
 
+
 class Noise(Attack):
     """
-    A weak attack that just picks a random point in the attacker's action space.
-    When combined with an attack bundling function, this can be used to implement
-    random search.
+    A weak attack that just picks a random point in the attacker's action
+    space. When combined with an attack bundling function, this can be used to
+    implement random search.
 
     References:
     https://arxiv.org/abs/1802.00420 recommends random search to help identify
         gradient masking
 
     https://openreview.net/forum?id=H1g0piA9tQ recommends using noise as part
-        of an attack building recipe combining many different optimizers to yield
-        a strong optimizer.
+        of an attack building recipe combining many different optimizers to
+        yield a strong optimizer.
 
     Args:
         model: Model
@@ -27,12 +28,12 @@ class Noise(Attack):
     """
 
     def __init__(self, model, dtype='float32', **kwargs):
-        
+
         super(Noise, self).__init__(model, dtype=dtype, **kwargs)
         self.feedable_kwargs = ('eps', 'clip_min', 'clip_max')
         self.structural_kwargs = ['ord']
 
-    def generate(self, x, **kwargs);
+    def generate(self, x, **kwargs):
         """
         Generate symbolic graph for adversarial examples and return.
 
@@ -43,11 +44,11 @@ class Noise(Attack):
 
         assert self.parse_params(**kwargs)
 
-        if self.ord != np.inf:
-            raise NotImplementedError(self.ord)
+        if self.ord != np.inf: raise NotImplementedError(self.ord)
         
         # TODO Check the dtype
-        eta = torch.FloatTensor(*x.shape).uniform_(-self.eps, self.eps, dtype=self.dtype)
+        eta = torch.FloatTensor(*x.shape).uniform_(-self.eps, self.eps, \
+                                dtype=self.dtype)
 
         adv_x = x + eta
 
@@ -56,10 +57,11 @@ class Noise(Attack):
 
         return adv_x
 
-    def parse_params(self, eps=0.3, ord=np.inf, clip_min=None, clip_max=None):
+    def parse_params(self, eps=0.3, order=np.inf, clip_min=None, 
+                     clip_max=None):
         """
-        Take in a dictionary of parameters and applies attack-specific checks before saving them
-        as attributes
+        Take in a dictionary of parameters and applies attack-specific checks 
+        before saving them as attributes.
 
         Attack-specific parameters:
             eps: (optional float) maximum distortion of the adversarial example
@@ -72,10 +74,10 @@ class Noise(Attack):
 
         # Save attack specific parameters
         self.eps = eps
-        self.ord = ord
+        self.ord = order
         self.clip_min = clip_min
-        self.clip_max - clip_max
+        self.clip_max = clip_max
 
         # Check if order of the norm is acceptable given current implementation
-        if self.ord not in [np.inf]:
-            raise ValueError("Norm order must be in np.inf")
+        if self.ord not in [np.inf]: raise ValueError("Norm order must be in "
+                                                      "np.inf")
