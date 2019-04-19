15c584337344cf49cda1b5ffc111e5b901f59adc
npapernot <papernot@google.com>
2019-04-19 16:31:13 +0000

    autopep8

--- tutorials/future/torch/mnist_tutorial.py
+++ tutorials/future/torch/mnist_tutorial.py
@@ -13,20 +13,21 @@ FLAGS = flags.FLAGS
 
 class CNN(torch.nn.Module):
   """Basic CNN architecture."""
+
   def __init__(self, in_channels=1):
-      super(CNN, self).__init__()
-      self.conv1 = nn.Conv2d(in_channels, 64, 8, 1)
-      self.conv2 = nn.Conv2d(64, 128, 6, 2)
-      self.conv3 = nn.Conv2d(128, 128, 5, 2)
-      self.fc = nn.Linear(128*2*2, 10)
+    super(CNN, self).__init__()
+    self.conv1 = nn.Conv2d(in_channels, 64, 8, 1)
+    self.conv2 = nn.Conv2d(64, 128, 6, 2)
+    self.conv3 = nn.Conv2d(128, 128, 5, 2)
+    self.fc = nn.Linear(128*2*2, 10)
 
   def forward(self, x):
-      x = F.relu(self.conv1(x))
-      x = F.relu(self.conv2(x))
-      x = F.relu(self.conv3(x))
-      x = x.view(-1, 128*2*2)
-      x = self.fc(x)
-      return x
+    x = F.relu(self.conv1(x))
+    x = F.relu(self.conv2(x))
+    x = F.relu(self.conv3(x))
+    x = x.view(-1, 128*2*2)
+    x = self.fc(x)
+    return x
 
 
 def ld_mnist():
@@ -85,6 +86,7 @@ def main(_):
   print('test acc on clean examples (%): {:.3f}'.format(report.correct / report.nb_test * 100.))
   print('test acc on FGM adversarial examples (%): {:.3f}'.format(report.correct_fgm / report.nb_test * 100.))
   print('test acc on PGD adversarial examples (%): {:.3f}'.format(report.correct_pgd / report.nb_test * 100.))
+
 
 if __name__ == '__main__':
   flags.DEFINE_integer('nb_epochs', 8, 'Number of epochs.')
