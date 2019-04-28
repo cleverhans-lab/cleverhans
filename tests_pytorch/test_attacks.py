16c1d32cc198488ccdd91655cb6045cbf748221f
michaelshiyu <michaelshiyu3@gmail.com>
2019-04-28 16:43:55 -0400

    Add tests for FGM

--- tests_pytorch/test_attacks.py
+++ tests_pytorch/test_attacks.py
@@ -4,6 +4,9 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import unittest
+
+import numpy as np
 from nose.plugins.skip import SkipTest
 import torch
 import torch.nn.functional as F
@@ -34,18 +37,64 @@ class CommonAttackProperties(CleverHansTest):
 
     super(CommonAttackProperties, self).setUp()
     self.model = SimpleModel(n_in=2, n_hidden=3, n_out=2)
-
-  def test_attack_can_be_called_with_different_settings(self):
-    raise NotImplementedError('must be implemented by a subclass')
+    self.x = torch.randn(10, 2)
 
 class TestFastGradientMethod(CommonAttackProperties):
+  """
+  Tests for FGM. Note that the core method of FGM, the optimize_linear function, is actually
+  tested in test_utils. So there's no need for repeating those tests here.
+  """
 
   def setUp(self):
     super(TestFastGradientMethod, self).setUp()
     self.attack = fast_gradient_method
+    self.ord_list = [1, 2, np.inf]
+
+  def test_invalid_input(self):
+    x = torch.tensor([[-2., 3.]])
+    for ord in self.ord_list:
+      self.assertRaises(
+          AssertionError, self.attack, model_fn=self.model, x=x, eps=.1,
+          ord=ord, clip_min=-1., clip_max=1., sanity_checks=True
+          )
+
+  def test_invalid_eps(self):
+    for ord in self.ord_list:
+      self.assertRaises(AssertionError, self.attack, model_fn=self.model, x=self.x, eps=-.1, ord=ord)
+
+  def test_eps_equals_zero(self):
+    for ord in self.ord_list:
+      self.assertClose(self.attack(model_fn=self.model, x=self.x, eps=0, ord=ord), self.x)
+
+  def test_eps(self):
+    pass
+
+  def test_clips(self):
+    normalized_x = torch.rand(10, 2)
+    clip_min = -1.
+    clip_max = 1.
+    for ord in self.ord_list:
+      x_adv = self.attack(
+          model_fn=self.model, x=normalized_x, eps=.3, ord=ord,
+          clip_min=clip_min, clip_max=clip_max
+          )
+      self.assertTrue(torch.all(x_adv <= clip_max))
+      self.assertTrue(torch.all(x_adv >= clip_min))
+
+  def test_invalid_clips(self):
+    clip_min = .5
+    clip_max = -.5
+    for ord in self.ord_list:
+      self.assertRaises(
+          AssertionError, self.attack, model_fn=self.model, x=self.x, eps=.1,
+          ord=ord, clip_min=clip_min, clip_max=clip_max
+          )
 
 class TestProjectedGradientMethod(CommonAttackProperties):
 
   def setUp(self):
     super(TestProjectedGradientMethod, self).setUp()
     self.attack = projected_gradient_descent
+
+if __name__ == "__main__":
+  unittest.main()
--- tests_pytorch/test_utils.py
+++ tests_pytorch/test_utils.py
@@ -9,9 +9,17 @@ class TestOptimizeLinear(CleverHansTest):
   """
   Identical to the TestOptimizeLinear in tests_tf/test_attacks.
   """
+  def setUp(self):
+    super(TestOptimizeLinear, self).setUp()
+    self.fn = utils_pytorch.optimize_linear
+    self.rand_grad = torch.rand(10, 3, 2)
+    self.red_ind = list(range(1, len(self.rand_grad.size())))
+    # eps needs to be nonnegative
+    self.eps_list = [0, .1, 1., 3]
+
   def test_optimize_linear_linf(self):
     grad = torch.tensor([[1., -2.]])
-    eta = utils_pytorch.optimize_linear(grad, eps=1., ord=np.inf)
+    eta = self.fn(grad, eps=1., ord=np.inf)
     objective = torch.sum(grad * eta)
 
     self.assertEqual(grad.size(), eta.size())
@@ -20,7 +28,7 @@ class TestOptimizeLinear(CleverHansTest):
 
   def test_optimize_linear_l2(self):
     grad = torch.tensor([[.5 ** .5, -.5 ** .5]])
-    eta = utils_pytorch.optimize_linear(grad, eps=1., ord=2)
+    eta = self.fn(grad, eps=1., ord=2)
     objective = torch.sum(grad * eta)
 
     self.assertEqual(grad.size(), eta.size())
@@ -29,7 +37,7 @@ class TestOptimizeLinear(CleverHansTest):
 
   def test_optimize_linear_l1(self):
     grad = torch.tensor([[1., -2.]])
-    eta = utils_pytorch.optimize_linear(grad, eps=1., ord=1)
+    eta = self.fn(grad, eps=1., ord=1)
     objective = torch.sum(grad * eta)
 
     self.assertEqual(grad.size(), eta.size())
@@ -38,9 +46,33 @@ class TestOptimizeLinear(CleverHansTest):
 
   def test_optimize_linear_l1_ties(self):
     grad = torch.tensor([[2., -2.]])
-    eta = utils_pytorch.optimize_linear(grad, eps=1., ord=1)
+    eta = self.fn(grad, eps=1., ord=1)
     objective = torch.sum(grad * eta)
 
     self.assertEqual(grad.size(), eta.size())
     self.assertClose(objective, 2.)
     self.assertClose(eta.abs().sum(), 1.)
+
+  def test_optimize_linear_linf_satisfies_norm_constraint(self):
+    for eps in self.eps_list:
+      eta = self.fn(self.rand_grad, eps=eps, ord=np.inf)
+      self.assertClose(eta.abs(), eps)
+
+  def test_optimize_linear_l1_satisfies_norm_constraint(self):
+    for eps in self.eps_list:
+      eta = self.fn(self.rand_grad, eps=eps, ord=1)
+      norm = eta.abs().sum(dim=self.red_ind)
+      self.assertTrue(torch.all(norm == eps * torch.ones_like(norm)))
+
+  def test_optimize_linear_l2_satisfies_norm_constraint(self):
+    for eps in self.eps_list:
+      eta = self.fn(self.rand_grad, eps=eps, ord=2)
+      avoid_zero_div = torch.tensor(1e-12)
+      square = torch.max(
+          avoid_zero_div,
+          torch.sum(self.rand_grad ** 2, self.red_ind, keepdim=True)
+          )
+      norm = eta.pow(2).sum(dim=self.red_ind, keepdim=True).sqrt()
+      one_mask = (square <= avoid_zero_div).to(torch.float) * norm + \
+              (square > avoid_zero_div).to(torch.float)
+      self.assertTrue(torch.allclose(norm, eps * one_mask, rtol=1e-05, atol=1e-08))
