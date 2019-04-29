c89025c05adee5cacfe379747ecaf4111a6622a0
michaelshiyu <michaelshiyu3@gmail.com>
2019-04-29 17:02:24 -0400

    Add attack success rate tests for FGM and PGD

--- tests_pytorch/test_attacks.py
+++ tests_pytorch/test_attacks.py
@@ -36,8 +36,29 @@ class CommonAttackProperties(CleverHansTest):
       raise SkipTest()
 
     super(CommonAttackProperties, self).setUp()
-    self.model = SimpleModel(n_in=2, n_hidden=3, n_out=2)
-    self.x = torch.randn(10, 2)
+    dim1 = 2
+    self.model = SimpleModel(n_in=dim1, n_hidden=5, n_out=2)
+    self.x = torch.randn(100, dim1)
+    self.normalized_x = torch.rand(100, dim1) # truncated between [0, 1)
+    self.red_ind = list(range(1, len(self.x.size())))
+
+  def help_adv_examples_success_rate(self, **kwargs):
+    x_adv = self.attack(**kwargs)
+    _, ori_label = self.model(self.x).max(1)
+    _, adv_label = self.model(x_adv).max(1)
+    adv_acc = adv_label.eq(ori_label).sum().to(torch.float) / self.x.size(0)
+    self.assertLess(adv_acc, .5)
+
+  def help_targeted_adv_examples_success_rate(self, **kwargs):
+    y_target = torch.randint(low=0, high=2, size=(self.x.size(0),)) # a two-class problem
+    try:
+      x_adv = self.attack(y=y_target, targeted=True, **kwargs)
+    except NotImplementedError:
+      raise SkipTest()
+
+    _, adv_label = self.model(x_adv).max(1)
+    adv_success = adv_label.eq(y_target).sum().to(torch.float) / self.x.size(0)
+    self.assertGreater(adv_success, .7)
 
 class TestFastGradientMethod(CommonAttackProperties):
   """
@@ -49,6 +70,7 @@ class TestFastGradientMethod(CommonAttackProperties):
     super(TestFastGradientMethod, self).setUp()
     self.attack = fast_gradient_method
     self.ord_list = [1, 2, np.inf]
+    self.eps_list = [0, .1, .3, 1., 3]
 
   def test_invalid_input(self):
     x = torch.tensor([[-2., 3.]])
@@ -67,15 +89,18 @@ class TestFastGradientMethod(CommonAttackProperties):
       self.assertClose(self.attack(model_fn=self.model, x=self.x, eps=0, ord=ord), self.x)
 
   def test_eps(self):
-    pass
+    # test if the attack respects the norm constraint
+    # NOTE this has been tested with the optimize_linear function in test_utils, so duplicate tests are not needed here.
+    # Although, if ever switch the engine of the FGM method to some
+    # function other than optimize_linear. This test should be added.
+    raise SkipTest()
 
   def test_clips(self):
-    normalized_x = torch.rand(10, 2)
     clip_min = -1.
     clip_max = 1.
     for ord in self.ord_list:
       x_adv = self.attack(
-          model_fn=self.model, x=normalized_x, eps=.3, ord=ord,
+          model_fn=self.model, x=self.normalized_x, eps=.3, ord=ord,
           clip_min=clip_min, clip_max=clip_max
           )
       self.assertTrue(torch.all(x_adv <= clip_max))
@@ -90,11 +115,69 @@ class TestFastGradientMethod(CommonAttackProperties):
           ord=ord, clip_min=clip_min, clip_max=clip_max
           )
 
+  def test_adv_example_success_rate_linf(self):
+    # use normalized_x to make sure the same eps gives uniformly high attack
+    # success rate across randomized tests
+    self.help_adv_examples_success_rate(
+        model_fn=self.model, x=self.normalized_x, ord=np.inf, eps=.5)
+
+  def test_targeted_adv_example_success_rate_linf(self):
+    self.help_targeted_adv_examples_success_rate(
+        model_fn=self.model, x=self.normalized_x, ord=np.inf, eps=.5)
+
+  def test_adv_example_success_rate_l1(self):
+    self.help_adv_examples_success_rate(
+        model_fn=self.model, x=self.normalized_x, ord=1, eps=.5)
+
+  def test_targeted_adv_example_success_rate_l1(self):
+    self.help_targeted_adv_examples_success_rate(
+        model_fn=self.model, x=self.normalized_x, ord=1, eps=.5)
+
+  def test_adv_example_success_rate_l2(self):
+    self.help_adv_examples_success_rate(
+        model_fn=self.model, x=self.normalized_x, ord=2, eps=.5)
+
+  def test_targeted_adv_example_success_rate_l2(self):
+    self.help_targeted_adv_examples_success_rate(
+        model_fn=self.model, x=self.normalized_x, ord=2, eps=.5)
+
 class TestProjectedGradientMethod(CommonAttackProperties):
 
   def setUp(self):
     super(TestProjectedGradientMethod, self).setUp()
     self.attack = projected_gradient_descent
+
+  def test_adv_example_success_rate_linf(self):
+    # use normalized_x to make sure the same eps gives uniformly high attack
+    # success rate across randomized tests
+    self.help_adv_examples_success_rate(
+        model_fn=self.model, x=self.normalized_x, ord=np.inf,
+        eps=.5, eps_iter=.05, nb_iter=10)
+
+  def test_targeted_adv_example_success_rate_linf(self):
+    self.help_targeted_adv_examples_success_rate(
+        model_fn=self.model, x=self.normalized_x, ord=np.inf,
+        eps=.5, eps_iter=.05, nb_iter=10)
+
+  def test_adv_example_success_rate_l1(self):
+    self.help_adv_examples_success_rate(
+        model_fn=self.model, x=self.normalized_x, ord=1,
+        eps=.5, eps_iter=.05, nb_iter=10)
+
+  def test_targeted_adv_example_success_rate_l1(self):
+    self.help_targeted_adv_examples_success_rate(
+        model_fn=self.model, x=self.normalized_x, ord=1,
+        eps=.5, eps_iter=.05, nb_iter=10)
+
+  def test_adv_example_success_rate_l2(self):
+    self.help_adv_examples_success_rate(
+        model_fn=self.model, x=self.normalized_x, ord=2,
+        eps=.5, eps_iter=.05, nb_iter=10)
+
+  def test_targeted_adv_example_success_rate_l2(self):
+    self.help_targeted_adv_examples_success_rate(
+        model_fn=self.model, x=self.normalized_x, ord=2,
+        eps=.5, eps_iter=.05, nb_iter=10)
 
 if __name__ == "__main__":
   unittest.main()
