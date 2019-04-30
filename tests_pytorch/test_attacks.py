8f40cea099e58ed670d6ba66dbffbca0c7399584
michaelshiyu <michaelshiyu3@gmail.com>
2019-04-30 11:01:52 -0400

    Adjust test settings

--- tests_pytorch/test_attacks.py
+++ tests_pytorch/test_attacks.py
@@ -4,12 +4,9 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import unittest
-
 import numpy as np
 from nose.plugins.skip import SkipTest
 import torch
-import torch.nn.functional as F
 
 from cleverhans.devtools.checks import CleverHansTest
 from cleverhans.future.torch.attacks.fast_gradient_method import fast_gradient_method
@@ -17,15 +14,15 @@ from cleverhans.future.torch.attacks.projected_gradient_descent import projected
 
 class SimpleModel(torch.nn.Module):
 
-  def __init__(self, n_in, n_hidden, n_out):
+  def __init__(self):
     super(SimpleModel, self).__init__()
-    self.fc1 = torch.nn.Linear(n_in, n_hidden)
-    self.fc2 = torch.nn.Linear(n_hidden, n_out)
+    self.w1 = torch.tensor([[1.5, .3], [-2, .3]])
+    self.w2 = torch.tensor([[-2.4, 1.2], [.5, -2.3]])
 
   def forward(self, x):
-    x = self.fc1(x)
-    x = F.relu(x)
-    x = self.fc2(x)
+    x = torch.matmul(x, self.w1)
+    x = torch.sigmoid(x)
+    x = torch.matmul(x, self.w2)
     return x
 
 class CommonAttackProperties(CleverHansTest):
@@ -36,28 +33,28 @@ class CommonAttackProperties(CleverHansTest):
       raise SkipTest()
 
     super(CommonAttackProperties, self).setUp()
-    dim1 = 2
-    self.model = SimpleModel(n_in=dim1, n_hidden=5, n_out=2)
-    self.x = torch.randn(100, dim1)
-    self.normalized_x = torch.rand(100, dim1) # truncated between [0, 1)
+    self.model = SimpleModel()
+    self.x = torch.randn(100, 2)
+    self.normalized_x = torch.rand(100, 2) # truncated between [0, 1)
     self.red_ind = list(range(1, len(self.x.size())))
 
   def help_adv_examples_success_rate(self, **kwargs):
-    x_adv = self.attack(**kwargs)
-    _, ori_label = self.model(self.x).max(1)
+    x_adv = self.attack(model_fn=self.model, x=self.normalized_x, **kwargs)
+    _, ori_label = self.model(self.normalized_x).max(1)
     _, adv_label = self.model(x_adv).max(1)
-    adv_acc = adv_label.eq(ori_label).sum().to(torch.float) / self.x.size(0)
+    adv_acc = adv_label.eq(ori_label).sum().to(torch.float) / self.normalized_x.size(0)
     self.assertLess(adv_acc, .5)
 
   def help_targeted_adv_examples_success_rate(self, **kwargs):
-    y_target = torch.randint(low=0, high=2, size=(self.x.size(0),)) # a two-class problem
+    y_target = torch.randint(low=0, high=2, size=(self.normalized_x.size(0),)) # a two-class problem
     try:
-      x_adv = self.attack(y=y_target, targeted=True, **kwargs)
+      x_adv = self.attack(
+          model_fn=self.model, x=self.normalized_x, y=y_target, targeted=True, **kwargs)
     except NotImplementedError:
       raise SkipTest()
 
     _, adv_label = self.model(x_adv).max(1)
-    adv_success = adv_label.eq(y_target).sum().to(torch.float) / self.x.size(0)
+    adv_success = adv_label.eq(y_target).sum().to(torch.float) / self.normalized_x.size(0)
     self.assertGreater(adv_success, .7)
 
 class TestFastGradientMethod(CommonAttackProperties):
@@ -119,27 +116,27 @@ class TestFastGradientMethod(CommonAttackProperties):
     # use normalized_x to make sure the same eps gives uniformly high attack
     # success rate across randomized tests
     self.help_adv_examples_success_rate(
-        model_fn=self.model, x=self.normalized_x, ord=np.inf, eps=.5)
+        ord=np.inf, eps=.5, clip_min=-5, clip_max=5)
 
   def test_targeted_adv_example_success_rate_linf(self):
     self.help_targeted_adv_examples_success_rate(
-        model_fn=self.model, x=self.normalized_x, ord=np.inf, eps=.5)
+        ord=np.inf, eps=.5, clip_min=-5, clip_max=5)
 
   def test_adv_example_success_rate_l1(self):
     self.help_adv_examples_success_rate(
-        model_fn=self.model, x=self.normalized_x, ord=1, eps=.5)
+        ord=1, eps=.5, clip_min=-5, clip_max=5)
 
   def test_targeted_adv_example_success_rate_l1(self):
     self.help_targeted_adv_examples_success_rate(
-        model_fn=self.model, x=self.normalized_x, ord=1, eps=.5)
+        ord=1, eps=.5, clip_min=-5, clip_max=5)
 
   def test_adv_example_success_rate_l2(self):
     self.help_adv_examples_success_rate(
-        model_fn=self.model, x=self.normalized_x, ord=2, eps=.5)
+        ord=2, eps=.5, clip_min=-5, clip_max=5)
 
   def test_targeted_adv_example_success_rate_l2(self):
     self.help_targeted_adv_examples_success_rate(
-        model_fn=self.model, x=self.normalized_x, ord=2, eps=.5)
+        ord=2, eps=.5, clip_min=-5, clip_max=5)
 
 class TestProjectedGradientMethod(CommonAttackProperties):
 
@@ -151,33 +148,30 @@ class TestProjectedGradientMethod(CommonAttackProperties):
     # use normalized_x to make sure the same eps gives uniformly high attack
     # success rate across randomized tests
     self.help_adv_examples_success_rate(
-        model_fn=self.model, x=self.normalized_x, ord=np.inf,
-        eps=.5, eps_iter=.05, nb_iter=10)
+        ord=np.inf, eps=.5, clip_min=-5, clip_max=5, eps_iter=.05, nb_iter=20)
 
   def test_targeted_adv_example_success_rate_linf(self):
     self.help_targeted_adv_examples_success_rate(
-        model_fn=self.model, x=self.normalized_x, ord=np.inf,
-        eps=.5, eps_iter=.05, nb_iter=10)
+        ord=np.inf, eps=.5, clip_min=-5, clip_max=5, eps_iter=.05, nb_iter=20)
 
   def test_adv_example_success_rate_l1(self):
-    self.help_adv_examples_success_rate(
-        model_fn=self.model, x=self.normalized_x, ord=1,
-        eps=.5, eps_iter=.05, nb_iter=10)
+    try:
+      self.help_adv_examples_success_rate(
+          ord=1, eps=.5, clip_min=-5, clip_max=5, eps_iter=.05, nb_iter=20)
+    except NotImplementedError:
+      raise SkipTest()
 
   def test_targeted_adv_example_success_rate_l1(self):
-    self.help_targeted_adv_examples_success_rate(
-        model_fn=self.model, x=self.normalized_x, ord=1,
-        eps=.5, eps_iter=.05, nb_iter=10)
+    try:
+      self.help_targeted_adv_examples_success_rate(
+          ord=1, eps=.5, clip_min=-5, clip_max=5, eps_iter=.05, nb_iter=20)
+    except NotImplementedError:
+      raise SkipTest()
 
   def test_adv_example_success_rate_l2(self):
     self.help_adv_examples_success_rate(
-        model_fn=self.model, x=self.normalized_x, ord=2,
-        eps=.5, eps_iter=.05, nb_iter=10)
+        ord=2, eps=.5, clip_min=-5, clip_max=5, eps_iter=.05, nb_iter=20)
 
   def test_targeted_adv_example_success_rate_l2(self):
     self.help_targeted_adv_examples_success_rate(
-        model_fn=self.model, x=self.normalized_x, ord=2,
-        eps=.5, eps_iter=.05, nb_iter=10)
-
-if __name__ == "__main__":
-  unittest.main()
+        ord=2, eps=.5, clip_min=-5, clip_max=5, eps_iter=.05, nb_iter=20)
