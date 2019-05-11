d434f5552063d01c1b026387808b8c0ff8dd946b
iarunava <learningdroidarunava@gmail.com>
2019-05-11 18:46:34 +0530

    Updated

--- cleverhans/future/torch/attacks/semantic.py
+++ cleverhans/future/torch/attacks/semantic.py
@@ -2,9 +2,7 @@
 Semantic adversarial Examples
 """
 
-from cleverhans.attacks.attack import Attack
-
-class Semantic(Attack):
+def semantic(x, center=True, max_val=1.):
   """
   Semantic adversarial examples.
   
@@ -15,24 +13,12 @@ class Semantic(Attack):
 
   Arguments
   ---------
-  net : nn.Module, optional
-        The model on which to perform the attack.
   center : bool
            If true, assumes data has 0 mean so the negative image is just negation.
            If false, assumes data is in interval [0, max_val]
   max_val : float
             Maximum value allowed in the input data.
-  kwargs : dict, optional
-           Any additional arguments
   """
 
-  def __init__(self, net, center=True, max_val=1., **kwargs):
-    super().__init__(net, **kwargs)
-    self.center = center
-    self.max_val = max_val
-
-  def generate(self, x, **kwargs):
-    if self.center:
-      return x*-1
-    return self.max_val - x
-
+  if self.center: return x*-1
+  return self.max_val - x
