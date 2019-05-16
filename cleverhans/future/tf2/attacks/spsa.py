69cfabbb87df9cbc9bf4fc0d576b3c0d500e0243
LorenzHW <lorenzhofmann.w@gmail.com>
2019-05-16 13:11:03 +0200

    Removing Tensor Optimzier + replacing nest with tf.nest

--- cleverhans/future/tf2/attacks/spsa.py
+++ cleverhans/future/tf2/attacks/spsa.py
@@ -55,102 +55,7 @@ def spsa(model_fn, x, y, eps, nb_iter, clip_min=None, clip_max=None, targeted=Fa
   return adv_x
 
 
-class TensorOptimizer(object):
-  """Optimizer for Tensors rather than tf.Variables.
-
-  TensorOptimizers implement optimizers where the values being optimized
-  are ordinary Tensors, rather than Variables. TF Variables can have strange
-  behaviors when being assigned multiple times within a single sess.run()
-  call, particularly in Distributed TF, so this avoids thinking about those
-  issues. These are helper classes for the `projected_optimization`
-  method. Apart from not using Variables, they follow an interface very
-  similar to tf.Optimizer.
-  """
-
-  def _compute_gradients(self, loss_fn, x, unused_optim_state):
-    """Compute a new value of `x` to minimize `loss_fn`.
-
-    Args:
-        loss_fn: a callable that takes `x`, a batch of images, and returns
-            a batch of loss values. `x` will be optimized to minimize
-            `loss_fn(x)`.
-        x: A list of Tensors, the values to be updated. This is analogous
-            to the `var_list` argument in standard TF Optimizer.
-        unused_optim_state: A (possibly nested) dict, containing any state
-            info needed for the optimizer.
-
-    Returns:
-        new_x: A list of Tensors, the same length as `x`, which are updated
-        new_optim_state: A dict, with the same structure as `optim_state`,
-            which have been updated.
-    """
-
-    # Assumes `x` is a list,
-    # and contains a tensor representing a batch of images
-    assert len(x) == 1 and isinstance(x, list), \
-      'x should be a list and contain only one image tensor'
-    x = x[0]
-    loss = tf.reduce_mean(loss_fn(x), axis=0)
-    return tf.gradients(loss, x)
-
-  def _apply_gradients(self, grads, x, optim_state):
-    """
-    Given a gradient, make one optimization step.
-
-    :param grads: list of tensors, same length as `x`, containing the corresponding gradients
-    :param x: list of tensors to update
-    :param optim_state: dict
-
-    Returns:
-      new_x: list of tensors, updated version of `x`
-      new_optim_state: dict, updated version of `optim_state`
-    """
-    raise NotImplementedError("_apply_gradients should be defined in each subclass")
-
-  def minimize(self, loss_fn, x, optim_state):
-    """
-    Analogous to tf.Optimizer.minimize
-
-    :param loss_fn: tf Tensor, representing the loss to minimize
-    :param x: list of Tensor, analogous to tf.Optimizer's var_list
-    :param optim_state: A possibly nested dict, containing any optimizer state.
-
-    Returns:
-      new_x: list of Tensor, updated version of `x`
-      new_optim_state: dict, updated version of `optim_state`
-    """
-    grads = self._compute_gradients(loss_fn, x, optim_state)
-    return self._apply_gradients(grads, x, optim_state)
-
-  def init_state(self, x):
-    """Returns the initial state of the optimizer.
-
-    Args:
-        x: A list of Tensors, which will be optimized.
-
-    Returns:
-        A dictionary, representing the initial state of the optimizer.
-    """
-    raise NotImplementedError("init_state should be defined in each subclass")
-
-
-class TensorGradientDescent(TensorOptimizer):
-  """Vanilla Gradient Descent TensorOptimizer."""
-
-  def __init__(self, lr):
-    self._lr = lr
-
-  def init_state(self, x):
-    return {}
-
-  def _apply_gradients(self, grads, x, optim_state):
-    new_x = [None] * len(x)
-    for i in range(len(x)):
-      new_x[i] = x[i] - self._lr * grads[i]
-    return new_x, optim_state
-
-
-class TensorAdam(TensorOptimizer):
+class TensorAdam(tf.optimizers.Adam):
   """The Adam optimizer defined in https://arxiv.org/abs/1412.6980."""
 
   def __init__(self, lr=0.001, beta1=0.9, beta2=0.999, epsilon=1e-9):
@@ -188,6 +93,21 @@ class TensorAdam(TensorOptimizer):
       u_hat = new_optim_state["u"][i] / (1. - tf.pow(self._beta2, t))
       new_x[i] = (x[i] - self._lr * m_hat / (tf.sqrt(u_hat) + self._epsilon))
     return new_x, new_optim_state
+
+  def minimize(self, loss_fn, x, optim_state):
+    """
+    Analogous to tf.Optimizer.minimize
+
+    :param loss_fn: tf Tensor, representing the loss to minimize
+    :param x: list of Tensor, analogous to tf.Optimizer's var_list
+    :param optim_state: A possibly nested dict, containing any optimizer state.
+
+    Returns:
+      new_x: list of Tensor, updated version of `x`
+      new_optim_state: dict, updated version of `optim_state`
+    """
+    grads = self._compute_gradients(loss_fn, x, optim_state)
+    return self._apply_gradients(grads, x, optim_state)
 
 
 class SPSAAdam(TensorAdam):
@@ -352,11 +272,10 @@ def projected_optimization(loss_fn, input_image, label, epsilon, num_steps, clip
   init_perturbation = project_perturbation(init_perturbation, epsilon, input_image,
                                            clip_min=clip_min, clip_max=clip_max)
   init_optim_state = optimizer.init_state([init_perturbation])
-  nest = tf.nest
 
   def loop_body(i, perturbation, flat_optim_state):
     """Update perturbation to input image."""
-    optim_state = nest.pack_sequence_as(structure=init_optim_state, flat_sequence=flat_optim_state)
+    optim_state = tf.nest.pack_sequence_as(structure=init_optim_state, flat_sequence=flat_optim_state)
 
     def wrapped_loss_fn(x):
       return loss_fn(input_image + x, label)
@@ -383,12 +302,12 @@ def projected_optimization(loss_fn, input_image, label, epsilon, num_steps, clip
       if early_stop:
         i = tf.cond(tf.less(loss, early_stop_loss_threshold), lambda: float(num_steps), lambda: i)
 
-    return i + 1, projected_perturbation, nest.flatten(new_optim_state)
+    return i + 1, projected_perturbation, tf.nest.flatten(new_optim_state)
 
   def cond(i, *_):
     return tf.less(i, num_steps)
 
-  flat_init_optim_state = nest.flatten(init_optim_state)
+  flat_init_optim_state = tf.nest.flatten(init_optim_state)
   _, final_perturbation, _ = tf.while_loop(
     cond,
     loop_body,
