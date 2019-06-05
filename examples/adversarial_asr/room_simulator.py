3ca888ff755dcecf2666c2cc0fa14cc9bab9148f
Yao Qin <yaq007@eng.ucsd.edu>
2019-06-05 11:27:30 -0400

    Update room_simulator.py

--- examples/adversarial_asr/room_simulator.py
+++ examples/adversarial_asr/room_simulator.py
@@ -46,6 +46,7 @@ room_settings = []
 num_rooms = 1500
 
 for i in range(num_rooms):
+	print('Saved room impulse:', i)
 	width = random.randint(3, 5)
 	length = random.randint(4, 6)
 	height = random.randint(2, 4)
