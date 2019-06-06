7ba0f1640db7cd7923134c6c5116f1daa6368dad
Yao Qin <yaq007@eng.ucsd.edu>
2019-06-06 13:17:17 -0400

    add tool.py

--- examples/adversarial_asr/tool.py
+++ examples/adversarial_asr/tool.py
@@ -69,14 +69,15 @@ class Transform(object):
     '''
     Return: PSD
     '''    
-    def __init__(self):
+    def __init__(self, window_size):
         self.scale = 8. / 3.
-        self.frame_length = int(FLAGS.window_size)
-        self.frame_step = int(FLAGS.window_size//4)
+        self.frame_length = int(window_size)
+        self.frame_step = int(window_size//4)
+        self.window_size = window_size
     
     def __call__(self, x, psd_max_ori):
         win = tf.contrib.signal.stft(x, self.frame_length, self.frame_step)
-        z = self.scale *tf.abs(win / FLAGS.window_size)
+        z = self.scale *tf.abs(win / self.window_size)
         psd = tf.square(z)
         PSD = tf.pow(10., 9.6) / tf.reshape(psd_max_ori, [-1, 1, 1]) * psd
         return PSD
