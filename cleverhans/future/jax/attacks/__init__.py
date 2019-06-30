8eea0d9cc56d2c6775d2e72bbb7dfdcf4d425d0a
npapernot <papernot@google.com>
2019-06-30 19:13:01 +0000

    add docstring and fix semantic attack in torch

--- cleverhans/future/jax/attacks/__init__.py
+++ cleverhans/future/jax/attacks/__init__.py
@@ -1,2 +1,3 @@
+"""Implementation of CleverHans attacks in JAX."""
 from cleverhans.future.jax.attacks.fast_gradient_method import fast_gradient_method
 from cleverhans.future.jax.attacks.projected_gradient_descent import projected_gradient_descent
