11bfdfd3ca0d2eb351a45bd4bd6cdbb8b781ac55
Cassidy Laidlaw <cassidy256@gmail.com>
2019-07-17 01:15:32 -0700

    Fix test_utils now that L_1 norm is implemented for clip_eta

--- cleverhans/future/torch/tests/test_utils.py
+++ cleverhans/future/torch/tests/test_utils.py
@@ -94,13 +94,9 @@ class TestOptimizeLinear(CleverHansTest):
     self.assertTrue(torch.all(clipped >= -.5))
 
   def test_clip_eta_l1(self):
-    self.assertRaises(
-        NotImplementedError, self.clip_eta, eta=self.rand_eta, norm=1, eps=.5)
-    
-    # TODO uncomment the actual test below after we have implemented the L1 attack
-    # clipped = self.clip_eta(eta=self.rand_eta, norm=1, eps=.5)
-    # norm = clipped.abs().sum(dim=self.red_ind)
-    # self.assertTrue(torch.all(norm <= .5001))
+    clipped = self.clip_eta(eta=self.rand_eta, norm=1, eps=.5)
+    norm = clipped.abs().sum(dim=self.red_ind)
+    self.assertTrue(torch.all(norm <= .5001))
 
   def test_clip_eta_l2(self):
     clipped = self.clip_eta(eta=self.rand_eta, norm=2, eps=.5)
