a4236e721877ad043e569921339f26e66230d9a8
Anshuman <anshuman@email.virginia.edu>
2019-10-04 18:29:08 +0000

    Adding MomentumIterativeMethod for tf2 (tested on tutorial scripts)

--- cleverhans/future/tf2/attacks/__init__.py
+++ cleverhans/future/tf2/attacks/__init__.py
@@ -1,3 +1,4 @@
 from cleverhans.future.tf2.attacks.fast_gradient_method import fast_gradient_method
 from cleverhans.future.tf2.attacks.projected_gradient_descent import projected_gradient_descent
 from cleverhans.future.tf2.attacks.spsa import spsa
+from cleverhans.future.tf2.attacks.momentum_iterative_method import momentum_iterative_method
