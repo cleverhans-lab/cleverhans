4d9633c24908ab3a95158c29b5d14c32fff54dae
michaelshiyu <michaelshiyu3@gmail.com>
2020-01-07 22:41:55 -0500

    Make CommonAttackProperties more general.

--- cleverhans/future/torch/tests/test_attacks.py
+++ cleverhans/future/torch/tests/test_attacks.py
@@ -43,28 +43,29 @@ class CommonAttackProperties(CleverHansTest):
     self.trivial_model = TrivialModel()
     self.x = torch.randn(100, 2)
     self.normalized_x = torch.rand(100, 2) # truncated between [0, 1)
-    self.red_ind = list(range(1, len(self.x.size())))
+    self.trivial_x = torch.randn(100, 1)
+    self.trivial_normalized_x = torch.rand(100, 1) # truncated between [0, 1)
     self.ord_list = [1, 2, np.inf]
 
-  def help_adv_examples_success_rate(self, model, rate=.5, **kwargs):
-    x_adv = self.attack(model_fn=model, x=self.normalized_x, **kwargs)
-    _, ori_label = model(self.normalized_x).max(1)
+  def help_adv_examples_success_rate(self, model, x, rate=.5, **kwargs):
+    x_adv = self.attack(model_fn=model, x=x, **kwargs)
+    _, ori_label = model(x).max(1)
     _, adv_label = model(x_adv).max(1)
     adv_acc = (
         adv_label.eq(ori_label).sum().to(torch.float)
-        / self.normalized_x.size(0))
+        / x.size(0))
     self.assertLess(adv_acc, rate)
 
-  def help_targeted_adv_examples_success_rate(self, model, rate=.7, **kwargs):
-    y_target = torch.randint(low=0, high=2, size=(self.normalized_x.size(0),))
+  def help_targeted_adv_examples_success_rate(self, model, x, rate=.7, **kwargs):
+    y_target = torch.randint(low=0, high=2, size=(x.size(0),))
     x_adv = self.attack(
-        model_fn=model, x=self.normalized_x,
+        model_fn=model, x=x,
         y=y_target, targeted=True, **kwargs)
 
     _, adv_label = model(x_adv).max(1)
     adv_success = (
         adv_label.eq(y_target).sum().to(torch.float)
-        / self.normalized_x.size(0))
+        / x.size(0))
     self.assertGreater(adv_success, rate)
 
 class TestFastGradientMethod(CommonAttackProperties):
@@ -130,27 +131,27 @@ class TestFastGradientMethod(CommonAttackProperties):
   def test_adv_example_success_rate_linf(self):
     # use normalized_x to make sure the same eps gives uniformly high attack
     # success rate across randomized tests
-    self.help_adv_examples_success_rate(
+    self.help_adv_examples_success_rate(x=self.normalized_x,
         model=self.model, norm=np.inf, **self.attack_param)
 
   def test_targeted_adv_example_success_rate_linf(self):
-    self.help_targeted_adv_examples_success_rate(
+    self.help_targeted_adv_examples_success_rate(x=self.normalized_x,
         model=self.model, norm=np.inf, **self.attack_param)
 
   def test_adv_example_success_rate_l1(self):
-    self.help_adv_examples_success_rate(
+    self.help_adv_examples_success_rate(x=self.normalized_x,
         model=self.model, norm=1, **self.attack_param)
 
   def test_targeted_adv_example_success_rate_l1(self):
-    self.help_targeted_adv_examples_success_rate(
+    self.help_targeted_adv_examples_success_rate(x=self.normalized_x,
         model=self.model, norm=1, **self.attack_param)
 
   def test_adv_example_success_rate_l2(self):
-    self.help_adv_examples_success_rate(
+    self.help_adv_examples_success_rate(x=self.normalized_x,
         model=self.model, norm=2, **self.attack_param)
 
   def test_targeted_adv_example_success_rate_l2(self):
-    self.help_targeted_adv_examples_success_rate(
+    self.help_targeted_adv_examples_success_rate(x=self.normalized_x,
         model=self.model, norm=2, **self.attack_param)
 
 class TestProjectedGradientMethod(CommonAttackProperties):
@@ -246,36 +247,36 @@ class TestProjectedGradientMethod(CommonAttackProperties):
   def test_adv_example_success_rate_linf(self):
     # use normalized_x to make sure the same eps gives uniformly high attack
     # success rate across randomized tests
-    self.help_adv_examples_success_rate(
+    self.help_adv_examples_success_rate(x=self.normalized_x,
         model=self.model, norm=np.inf, **self.attack_param)
 
   def test_targeted_adv_example_success_rate_linf(self):
-    self.help_targeted_adv_examples_success_rate(
+    self.help_targeted_adv_examples_success_rate(x=self.normalized_x,
         model=self.model, norm=np.inf, **self.attack_param)
 
   def test_adv_example_success_rate_l1(self):
     self.assertRaises(
         NotImplementedError, self.help_adv_examples_success_rate, model=self.model,
-      norm=1, **self.attack_param)
+      x=self.normalized_x, norm=1, **self.attack_param)
     # TODO uncomment the actual test below after we have implemented the L1 attack
-    # self.help_adv_examples_success_rate(
+    # self.help_adv_examples_success_rate(x=self.normalized_x,
     #     model=self.model, norm=1, **self.attack_param)
 
   def test_targeted_adv_example_success_rate_l1(self):
     self.assertRaises(
         NotImplementedError, self.help_targeted_adv_examples_success_rate,
-        model=self.model, norm=1, **self.attack_param)
+        x=self.normalized_x, model=self.model, norm=1, **self.attack_param)
     # TODO uncomment the actual test below after we have implemented the L1 attack
-    # self.help_targeted_adv_examples_success_rate(
+    # self.help_targeted_adv_examples_success_rate(x=self.normalized_x,
     #     model=self.model, norm=1, **self.attack_param)
 
   def test_adv_example_success_rate_l2(self):
     self.help_adv_examples_success_rate(model=self.model,
-        norm=2, **self.attack_param)
+        x=self.normalized_x, norm=2, **self.attack_param)
 
   def test_targeted_adv_example_success_rate_l2(self):
     self.help_targeted_adv_examples_success_rate(model=self.model,
-        norm=2, **self.attack_param)
+        x=self.normalized_x, norm=2, **self.attack_param)
 
   def test_do_not_reach_lp_boundary(self):
     for norm in self.ord_list:
@@ -375,11 +376,11 @@ class TestCarliniWagnerL2(CommonAttackProperties):
 
   def test_adv_example_success_rate(self):
     self.help_adv_examples_success_rate(model=self.model,
-      rate=.1, **self.attack_param)
+      x=self.normalized_x, rate=.1, **self.attack_param)
 
   def test_targeted_adv_example_success_rate(self):
     self.help_targeted_adv_examples_success_rate(model=self.model,
-      rate=.9, **self.attack_param)
+      x=self.normalized_x, rate=.9, **self.attack_param)
 
   def test_adv_examples_clipped_successfully(self):
     x_adv = self.attack(model_fn=self.model,
