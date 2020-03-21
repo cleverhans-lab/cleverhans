2ddbc92e3ce836bc98fc60b9ec54af45005c7906
cnocycle <github@marginalman.org>
2020-03-21 17:05:04 +0800

    fix issue #1150

--- cleverhans/future/tf2/attacks/__init__.py
+++ cleverhans/future/tf2/attacks/__init__.py
@@ -2,4 +2,4 @@ from cleverhans.future.tf2.attacks.fast_gradient_method import fast_gradient_met
 from cleverhans.future.tf2.attacks.projected_gradient_descent import projected_gradient_descent
 from cleverhans.future.tf2.attacks.spsa import spsa
 from cleverhans.future.tf2.attacks.madry_et_al import madry_et_al
-from cleverhans.future.tf2.basic_iterative_method import basic_iterative_method
+from cleverhans.future.tf2.attacks.basic_iterative_method import basic_iterative_method
