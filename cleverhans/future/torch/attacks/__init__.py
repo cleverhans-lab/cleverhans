4686054446f80371570bb7642a0e01ace89ca87a
Anshuman Suri <anshuman@email.virginia.edu>
2020-05-06 21:22:48 -0400

    Add HSJA for Pytorch

--- cleverhans/future/torch/attacks/__init__.py
+++ cleverhans/future/torch/attacks/__init__.py
@@ -4,3 +4,4 @@ from cleverhans.future.torch.attacks.projected_gradient_descent import projected
 from cleverhans.future.torch.attacks.noise import noise
 from cleverhans.future.torch.attacks.semantic import semantic
 from cleverhans.future.torch.attacks.spsa import spsa
+from cleverhans.future.torch.attacks.hop_skip_jump_attack import hop_skip_jump_attack
