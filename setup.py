1024e73f14810a04b4f49284f62ab253767f66bb
Jonas <jonas@cs.toronto.edu>
2021-02-02 11:46:21 +0800

    Updated setup.py for v4.0.0

--- setup.py
+++ setup.py
@@ -2,24 +2,22 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(name='cleverhans',
-      version='3.0.1',
-      url='https://github.com/tensorflow/cleverhans',
+      version='4.0.0',
+      url='https://github.com/cleverhans-lab/cleverhans',
       license='MIT',
       install_requires=[
           'nose',
           'pycodestyle',
           'scipy',
           'matplotlib',
-          "mnist ~= 0.2",
+          "mnist",
           "numpy",
           "tensorflow-probability",
           "joblib",
       ],
-      # Explicit dependence on TensorFlow is not supported.
-      # See https://github.com/tensorflow/tensorflow/issues/7166
       extras_require={
-          "tf": ["tensorflow>=1.0.0"],
-          "tf_gpu": ["tensorflow-gpu>=1.0.0"],
-          "pytorch": ["torch>=1.1.0", "torchvision==0.3.0"],
+          "jax": ["jax>=0.2.9"],
+          "tf": ["tensorflow>=2.4.0"],
+          "pytorch": ["torch>=1.7.0", "torchvision>=0.8.0"],
       },
       packages=find_packages())
