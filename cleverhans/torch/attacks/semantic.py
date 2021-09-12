6d27e45ee7f82f77d69c829ada7bbf8575bc966d
xu-weizhen <37538790+xu-weizhen@users.noreply.github.com>
2021-09-12 17:16:29 +0800

    Update semantic.py
    Correct errors.

--- cleverhans/torch/attacks/semantic.py
+++ cleverhans/torch/attacks/semantic.py
@@ -21,6 +21,6 @@ def semantic(x, center=True, max_val=1.0):
               Maximum value allowed in the input data.
     """
 
-    if self.center:
+    if center:
         return x * -1
-    return self.max_val - x
+    return max_val - x
